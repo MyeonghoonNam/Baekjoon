1db758bcbb09aeebcfc216d600335685651fbeb4
MyeongHoonNam <codenmh0822@naver.com>
2021-04-08 21:46:51 +0900

    문제 해결

--- 동적계획법/test.js
+++ 동적계획법/test.js
@@ -1,9 +1,7 @@
-const a = [1, [2, 3, 4, 5]];
-console.log(a);
-const b = a[1];
-console.log(b);
+const input = ["6","10 20 10 30 20 50"];
+const N = Number(input.shift())
+const nums = input.shift().split(" ").map(e => Number(e))
 
-b.sort((a, b) => b - a);
-
-console.log(a);
-console.log(b);
+console.log(input);
+console.log(N);
+console.log(nums);
