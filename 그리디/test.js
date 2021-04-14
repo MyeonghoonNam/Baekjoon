90422f0d2cae43337017f68a9586c2225ae7036c
MyeongHoonNam <codenmh0822@naver.com>
2021-04-14 20:59:33 +0900

    문제 해결

--- 그리디/test.js
+++ 그리디/test.js
@@ -1,24 +1,18 @@
-// const input = [[4], [2, 3, 1], [5, 2, 4, 1]];
+const input = [[4], [2, 3, 1], [5, 2, 4, 1]];
 
-// const N = input[0][0];
-// const dist = input[1];
-// const cost = input[2];
+const N = input[0][0];
+const dist = input[1];
+const cost = input[2];
 
-// let sum = 0n;
-// let minCost = BigInt(cost[0]);
+let sum = 0n;
+let minCost = BigInt(cost[0]);
 
-// for (let i = 0; i < N - 1; i++) {
-//   if (BigInt(cost[i]) < minCost) {
-//     minCost = BigInt(cost[i]);
-//   }
+for (let i = 0; i < N - 1; i++) {
+  if (BigInt(cost[i]) < minCost) {
+    minCost = BigInt(cost[i]);
+  }
 
-//   sum += minCost * BigInt(dist[i]);
-// }
-
-// console.log(sum.toString());
-
-const i = [1n, 2n];
-
-if (i[1] < 3n) {
-  console.log('hi');
+  sum += minCost * BigInt(dist[i]);
 }
+
+console.log(sum.toString());
