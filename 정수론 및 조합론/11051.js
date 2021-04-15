4c3d3579f38a715bbc9bd59cf0eb592f12a06e65
MyeonghoonNam <codenmh0822@naver.com>
2021-04-15 18:49:17 +0900

    문제 해결

---
+++ 정수론 및 조합론/11051.js
@@ -0,0 +1,32 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  const input = line.split(' ').map((el) => parseInt(el));
+  const N = input[0];
+  const K = input[1];
+
+  const dp = Array.from(new Array(N + 1), () => new Array(K + 1).fill(0));
+
+  for (let i = 0; i <= K; i++) {
+    dp[i][i] = 1;
+  }
+
+  for (let i = 0; i <= N; i++) {
+    dp[i][0] = 1;
+  }
+
+  for (let i = 2; i <= N; i++) {
+    for (let j = 1; j <= K; j++) {
+      dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+    }
+  }
+
+  console.log(dp[N][K]);
+}).on('close', () => {
+  process.exit();
+});
--- 정수론 및 조합론/test.js
+++ 정수론 및 조합론/test.js
@@ -1,7 +1,9 @@
-const input = [5, 2];
+const input = [1000, 999];
+
 const N = input[0];
 const K = input[1];
-const dp = Array.from(Array(N + 1), () => new Array(K + 1).fill(0));
+
+const dp = Array.from(new Array(N + 1), () => new Array(K + 1).fill(0));
 
 for (let i = 0; i <= K; i++) {
   dp[i][i] = 1;
@@ -13,17 +15,8 @@ for (let i = 0; i <= N; i++) {
 
 for (let i = 2; i <= N; i++) {
   for (let j = 1; j <= K; j++) {
-    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
   }
 }
 
-// console.log(BC(N, K));
 console.log(dp[N][K]);
-
-// function BC(n, k) {
-//   if (k === 0 || n === k) {
-//     return 1;
-//   } else {
-//     return BC(n - 1, k - 1) + BC(n - 1, k);
-//   }
-// }
