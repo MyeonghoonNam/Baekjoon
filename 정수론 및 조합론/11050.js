45ca2fbe14a47b268714033474ad24e62b7c39cd
MyeonghoonNam <codenmh0822@naver.com>
2021-04-15 18:34:13 +0900

    문제 해결

--- 정수론 및 조합론/11050.js
+++ 정수론 및 조합론/11050.js
@@ -11,19 +11,30 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', (line) => {
+  // 동적계획법을 이용한 이항계수 알고리즘
+
   const input = line.split(' ').map((el) => parseInt(el));
   const N = input[0];
   const K = input[1];
+  const dp = Array.from(new Array(N + 1), () => new Array(K + 1).fill(0));
 
-  console.log(BC(N, K));
+  // N === K => 1
+  for (let i = 0; i <= K; i++) {
+    dp[i][i] = 1;
+  }
 
-  function BC(n, k) {
-    if (n === k || k === 0) {
-      return 1;
-    } else {
-      return BC(n - 1, k - 1) + BC(n - 1, k);
+  // K === 0 => 1
+  for (let i = 0; i <= N; i++) {
+    dp[i][0] = 1;
+  }
+
+  for (let i = 2; i <= N; i++) {
+    for (let j = 1; j <= K; j++) {
+      dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
     }
   }
+
+  console.log(dp[N][K]);
 }).on('close', () => {
   process.exit();
 });
--- 정수론 및 조합론/test.js
+++ 정수론 및 조합론/test.js
@@ -1,13 +1,29 @@
 const input = [5, 2];
 const N = input[0];
 const K = input[1];
+const dp = Array.from(Array(N + 1), () => new Array(K + 1).fill(0));
 
-console.log(BC(N, K));
+for (let i = 0; i <= K; i++) {
+  dp[i][i] = 1;
+}
 
-function BC(n, k) {
-  if (k === 0 || n === k) {
-    return 1;
-  } else {
-    return BC(n - 1, k - 1) + BC(n - 1, k);
+for (let i = 0; i <= N; i++) {
+  dp[i][0] = 1;
+}
+
+for (let i = 2; i <= N; i++) {
+  for (let j = 1; j <= K; j++) {
+    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
   }
 }
+
+// console.log(BC(N, K));
+console.log(dp[N][K]);
+
+// function BC(n, k) {
+//   if (k === 0 || n === k) {
+//     return 1;
+//   } else {
+//     return BC(n - 1, k - 1) + BC(n - 1, k);
+//   }
+// }
