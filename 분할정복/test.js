8109fa11ef18e4f39c04868ec277aa732a300015
MyeonghoonNam <codenmh0822@naver.com>
2021-04-21 15:16:07 +0900

    문제 해결

--- 분할정복/test.js
+++ 분할정복/test.js
@@ -1,52 +1,47 @@
 const input = [
-  '8',
-  '11000011',
-  '11000011',
-  '00001100',
-  '00001100',
-  '10001111',
-  '01001111',
-  '00111111',
-  '00111111',
+  [8],
+  [1, 1, 1, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 1, 1],
+  [1, 1, 1, 1, 0, 0, 1, 1],
 ];
 
-const N = parseInt(input[0]);
-let whiteCount = 0;
-let blueCount = 0;
-
+const N = input[0][0];
 const board = input.slice(1);
+let result = '';
 
 Solution(0, 0, N);
-
-const result = `${whiteCount}\n${blueCount}`;
 
 console.log(result);
 
 function Solution(row, col, size) {
-  if (ColorCheck(row, col, size)) {
-    if (board[row][col] === '0') {
-      whiteCount++;
-    } else {
-      blueCount++;
-    }
-
+  if (isPossible(row, col, size)) {
+    result += board[row][col];
     return;
   }
 
   const newSize = size / 2;
 
+  result += '(';
+
   Solution(row, col, newSize); // 2사분면
   Solution(row, col + newSize, newSize); // 1사분면
   Solution(row + newSize, col, newSize); // 3사분면
   Solution(row + newSize, col + newSize, newSize); // 4사분면
+
+  result += ')';
 }
 
-function ColorCheck(row, col, size) {
-  const color = board[row][col];
+function isPossible(row, col, size) {
+  const value = board[row][col];
 
   for (let i = row; i < row + size; i++) {
     for (let j = col; j < col + size; j++) {
-      if (board[i][j] !== color) {
+      if (board[i][j] !== value) {
         return false;
       }
     }
