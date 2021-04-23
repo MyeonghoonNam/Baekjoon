1c09f471e578614ac3203fc3c5e7144e776fb691
MyeongHoonNam <codenmh0822@naver.com>
2021-04-23 23:32:12 +0900

    문제 해결

--- 그래프/test.js
+++ 그래프/test.js
@@ -1,93 +1,74 @@
-// const input = [
-//   '6 4',
-//   '0 0 0 0 0 0',
-//   '0 0 0 0 0 0',
-//   '0 0 0 0 0 0',
-//   '0 0 0 0 0 1',
-// ];
-// const input = [
-//   '6 4',
-//   '0 -1 0 0 0 0',
-//   '-1 0 0 0 0 0',
-//   '0 0 0 0 0 0',
-//   '0 0 0 0 0 1',
-// ];
 const input = [
-  '6 4',
-  '1 -1 0 0 0 0',
-  '0 -1 0 0 0 0',
-  '0 0 0 0 -1 0',
-  '0 0 0 0 -1 1',
+  '3',
+  '8',
+  '0 0',
+  '7 0',
+  '100',
+  '0 0',
+  '30 50',
+  '10',
+  '1 1',
+  '1 1',
 ];
 
-const [M, N] = input[0].split(' ').map((el) => parseInt(el));
-input.shift();
+const T = parseInt(input.shift());
 
-const box = new Array(N);
-for (let i = 0; i < N; i++) {
-  box[i] = input[i].split(' ').map((el) => parseInt(el));
+let L = 0;
+let visited = [];
+
+for (let i = 0; i < T; i++) {
+  L = parseInt(input.shift());
+  visited = Array.from(new Array(L), () => new Array(L).fill(false));
+
+  const start = input
+    .shift()
+    .split(' ')
+    .map((el) => parseInt(el));
+
+  const end = input
+    .shift()
+    .split(' ')
+    .map((el) => parseInt(el));
+
+  Bfs(start, end);
 }
 
-BFS();
-
-function BFS() {
+function Bfs(start, end) {
   const q = [];
-  let day = 0;
-  let zeroCount = 0;
+  let count = 0;
 
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
+  q.push([...start, count]);
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (box[i][j] === 1) {
-        q.push([i, j, day]); // 이미 익은 토마토 이므로 day = 0
-      } else if (box[i][j] === 0) {
-        zeroCount++;
+  while (q.length !== 0) {
+    const current = q.shift();
+    const start_X = current[0];
+    const start_Y = current[1];
+    count = current[2];
+
+    if (start_X === end[0] && start_Y === end[1]) {
+      console.log(count);
+      return;
+    }
+
+    const dx = [-2, -2, -1, 1, 2, 2, 1, -1, -2];
+    const dy = [-1, 1, 2, 2, 1, -1, -2, -2, -1];
+
+    for (let i = 0; i < 9; i++) {
+      const next_X = start_X + dx[i];
+      const next_Y = start_Y + dy[i];
+
+      if (CheckRange(next_X, next_Y) && !visited[next_X][next_Y]) {
+        visited[next_X][next_Y] = true;
+        q.push([next_X, next_Y, count + 1]);
       }
     }
-  }
-
-  let head = 0;
-  while (q.length > head) {
-    const size = q.length - head;
-    for (let s = 0; s < size; s++) {
-      const current = q[head++];
-
-      const x = current[0];
-      const y = current[1];
-      day = current[2];
-
-      for (let i = 0; i < 4; i++) {
-        const next_X = x + dx[i];
-        const next_Y = y + dy[i];
-
-        if (next_X >= 0 && next_X < N && next_Y >= 0 && next_Y < M) {
-          if (box[next_X][next_Y] === 0) {
-            zeroCount--;
-            box[next_X][next_Y] = 1;
-            q.push([next_X, next_Y, day + 1]);
-          }
-        }
-      }
-    }
-  }
-
-  if (zeroCount !== 0) {
-    console.log(-1);
-  } else {
-    console.log(day);
   }
 }
 
-// function checkTomato() {
-//   for (let i = 0; i < N; i++) {
-//     for (let j = 0; j < M; j++) {
-//       if (box[i][j] === 0) {
-//         return false;
-//       }
-//     }
-//   }
-
-//   return true;
-// }
+function CheckRange(x, y) {
+  if (x >= 0 && x < L && y >= 0 && y < L) {
+    return true;
+  } else {
+    return false;
+  }
+}
