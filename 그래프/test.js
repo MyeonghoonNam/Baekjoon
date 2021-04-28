123f6ccdb15463830d28bbc1964f4818780a3c4e
MyeonghoonNam <codenmh0822@naver.com>
2021-04-28 20:32:34 +0900

    문제 보류

--- 그래프/test.js
+++ 그래프/test.js
@@ -1,38 +1,18 @@
 const input = ['5 17'];
 
-console.log(Solution(input));
-
 function Solution(input) {
-  const [N, K] = input[0].split(' ').map(Number);
-
   const MAX = 100000;
-  const time = new Array(MAX + 1).fill(-1);
+  const [N, K] = input[0].split(' ').map(Number);
+  const visited = new Array(MAX + 1).fill(false)
+  ;
+  let route = '';
 
   const q = [N];
-  time[N] = 0;
+  while(q.length > 0) {
+    const [pos, time] = q.shift();
 
-  let head = 0;
-  while (q.length) {
-    const idx = q[head++];
+    visited[pos] = true;
 
-    if (idx === K) {
-      break;
-    }
-
-    if (idx * 2 <= MAX && time[idx * 2] === -1) {
-      time[idx * 2] = time[idx];
-      q.push(idx * 2);
-    }
-
-    if (idx - 1 >= 0 && time[idx - 1] === -1) {
-      time[idx - 1] = time[idx] + 1;
-      q.push(idx - 1);
-    }
-
-    if (idx + 1 <= MAX && time[idx + 1] === -1) {
-      time[idx + 1] = time[idx] + 1;
-      q.push(idx + 1);
-    }
+    if(pos === K)
   }
-  return time[K];
 }
