932f8749e1053c7f157901598340f76baff54706
MyeongHoonNam <codenmh0822@naver.com>
2021-04-28 23:28:34 +0900

    문제 해결

--- 그래프/test.js
+++ 그래프/test.js
@@ -1,18 +1,59 @@
 const input = ['5 17'];
 
+const [time, route] = Solution(input);
+
+console.log(time);
+
+let result = '';
+for (let i = route.length - 1; i >= 0; i--) {
+  if (i === 0) {
+    result += route[i];
+    break;
+  }
+
+  result += `${route[i]} `;
+}
+
+console.log(result);
+
 function Solution(input) {
   const MAX = 100000;
   const [N, K] = input[0].split(' ').map(Number);
-  const visited = new Array(MAX + 1).fill(false)
-  ;
-  let route = '';
+  const visited = new Array(MAX + 1).fill(false);
+  const history = new Array(MAX + 1).fill(false);
+  const route = [];
 
-  const q = [N];
-  while(q.length > 0) {
+  const q = [[N, 0]];
+  while (q.length > 0) {
     const [pos, time] = q.shift();
 
-    visited[pos] = true;
+    if (pos === K) {
+      let idx = pos;
+      while (idx !== N) {
+        route.push(idx);
+        idx = history[idx];
+      }
 
-    if(pos === K)
+      route.push(N);
+      return [time, route];
+    }
+
+    if (pos - 1 >= 0 && !visited[pos - 1]) {
+      visited[pos - 1] = true;
+      history[pos - 1] = pos;
+      q.push([pos - 1, time + 1]);
+    }
+
+    if (pos + 1 <= MAX && !visited[pos + 1]) {
+      visited[pos + 1] = true;
+      history[pos + 1] = pos;
+      q.push([pos + 1, time + 1]);
+    }
+
+    if (pos * 2 <= MAX && !visited[pos * 2]) {
+      visited[pos * 2] = true;
+      history[pos * 2] = pos;
+      q.push([pos * 2, time + 1]);
+    }
   }
 }
