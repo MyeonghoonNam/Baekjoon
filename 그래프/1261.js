1a7d04263c97c5afecfd2d29c81ec03eea202cae
MyeongHoonNam <codenmh0822@naver.com>
2021-04-29 18:36:20 +0900

    문제 해결

--- 그래프/1261.js
+++ 그래프/1261.js
@@ -11,55 +11,50 @@ rl.on('line', (line) => {
   input.push(line);
 }).on('close', () => {
   // 구현
+  // 다익스트라 알고리즘 : visited를 안쓰고 가중치 기록 그래프 이용(dist)
   console.log(Solution(input));
 
   function Solution(input) {
     const [M, N] = input.shift().split(' ').map(Number);
 
-    const graph = Array.from(new Array(N + 1), () => new Array());
-
-    for (let i = 1; i <= N; i++) {
+    const graph = Array.from(new Array(N), () => new Array());
+    for (let i = 0; i < N; i++) {
       graph[i] = input.shift().split('').map(Number);
     }
 
-    const dist = Array.from(new Array(N + 1), () => new Array(M + 1).fill(-1));
+    const dist = Array.from(new Array(N), () => new Array(M).fill(Infinity));
 
-    for (let i = 1; i <= N; i++) {
-      for (let j = 1; j <= M; j++) {
-        if (graph[i][j - 1] === 0) {
-          dist[i][j] = 0;
-        }
-      }
-    }
+    Bfs(graph, dist, N, M);
 
-    const visited = Array.from(new Array(N + 1), () =>
-      new Array(M + 1).fill(false)
-    );
+    return dist[N - 1][M - 1];
+  }
 
-    // Bfs
-    const q = [[1, 1]];
+  function Bfs(graph, dist, N, M) {
+    const q = [[0, 0]];
+
+    dist[0][0] = 0;
 
     const dx = [-1, 1, 0, 0];
     const dy = [0, 0, -1, 1];
+
     while (q.length > 0) {
       const [x, y] = q.shift();
-
-      if (visited[x][y]) continue;
-
-      visited[x][y] = true;
 
       for (let i = 0; i < 4; i++) {
         const nx = x + dx[i];
         const ny = y + dy[i];
 
-        if (CheckRange(nx, ny, N, M) && !visited[nx][ny]) {
-          if (nx === N && ny === M) {
-            return dist[x][y];
-          } else if (dist[nx][ny] === -1) {
-            dist[nx][ny] = dist[x][y] + 1;
-            q.push([nx, ny]);
-          } else if (dist[nx][ny] === 0) {
-            q.push([nx, ny]);
+        if (CheckRange(nx, ny, N, M)) {
+          if (graph[nx][ny] === 1) {
+            if (dist[nx][ny] > dist[x][y]) {
+              dist[nx][ny] = dist[x][y] + 1;
+              q.push([nx, ny]);
+            }
+          } else if (graph[nx][ny] === 0) {
+            if (dist[nx][ny] > dist[x][y]) {
+              dist[nx][ny] = dist[x][y];
+              q.push([nx, ny]);
+            }
           }
         }
       }
@@ -67,7 +62,7 @@ rl.on('line', (line) => {
   }
 
   function CheckRange(x, y, N, M) {
-    if (x >= 1 && x <= N && y >= 1 && y <= M) {
+    if (x >= 0 && x < N && y >= 0 && y < M) {
       return true;
     } else {
       return false;
--- 그래프/test.js
+++ 그래프/test.js
@@ -1,55 +1,58 @@
 // const input = ['3 3', '011', '111', '110'];
-const input = ['4 2', '0001', '1000'];
+// const input = ['4 2', '0001', '1000'];
+const input = [
+  '6 6',
+  '001111',
+  '010000',
+  '001111',
+  '110001',
+  '011010',
+  '100010',
+];
 
 console.log(Solution(input));
 
 function Solution(input) {
   const [M, N] = input.shift().split(' ').map(Number);
 
-  const graph = Array.from(new Array(N + 1), () => new Array());
-
-  for (let i = 1; i <= N; i++) {
+  const graph = Array.from(new Array(N), () => new Array());
+  for (let i = 0; i < N; i++) {
     graph[i] = input.shift().split('').map(Number);
   }
 
-  const dist = Array.from(new Array(N + 1), () => new Array(M + 1).fill(-1));
+  const dist = Array.from(new Array(N), () => new Array(M).fill(Infinity));
 
-  for (let i = 1; i <= N; i++) {
-    for (let j = 1; j <= M; j++) {
-      if (graph[i][j - 1] === 0) {
-        dist[i][j] = 0;
-      }
-    }
-  }
+  Bfs(graph, dist, N, M);
 
-  const visited = Array.from(new Array(N + 1), () =>
-    new Array(M + 1).fill(false)
-  );
+  return dist[N - 1][M - 1];
+}
 
-  // Bfs
-  const q = [[1, 1]];
+function Bfs(graph, dist, N, M) {
+  const q = [[0, 0]];
+
+  dist[0][0] = 0;
 
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
+
   while (q.length > 0) {
     const [x, y] = q.shift();
-
-    if (visited[x][y]) continue;
-
-    visited[x][y] = true;
 
     for (let i = 0; i < 4; i++) {
       const nx = x + dx[i];
       const ny = y + dy[i];
 
-      if (CheckRange(nx, ny, N, M) && !visited[nx][ny]) {
-        if (nx === N && ny === M) {
-          return dist[x][y];
-        } else if (dist[nx][ny] === -1) {
-          dist[nx][ny] = dist[x][y] + 1;
-          q.push([nx, ny]);
-        } else if (dist[nx][ny] === 0) {
-          q.push([nx, ny]);
+      if (CheckRange(nx, ny, N, M)) {
+        if (graph[nx][ny] === 1) {
+          if (dist[nx][ny] > dist[x][y]) {
+            dist[nx][ny] = dist[x][y] + 1;
+            q.push([nx, ny]);
+          }
+        } else if (graph[nx][ny] === 0) {
+          if (dist[nx][ny] > dist[x][y]) {
+            dist[nx][ny] = dist[x][y];
+            q.push([nx, ny]);
+          }
         }
       }
     }
@@ -57,7 +60,7 @@ function Solution(input) {
 }
 
 function CheckRange(x, y, N, M) {
-  if (x >= 1 && x <= N && y >= 1 && y <= M) {
+  if (x >= 0 && x < N && y >= 0 && y < M) {
     return true;
   } else {
     return false;
