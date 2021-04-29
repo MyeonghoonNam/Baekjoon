90b1067caa284b8dfc9a5499a48a4aabfec5a800
MyeongHoonNam <codenmh0822@naver.com>
2021-04-29 21:52:45 +0900

    문제 해결

--- 동적계획법/11726.js
+++ 동적계획법/11726.js
@@ -11,4 +11,16 @@ rl.on('line', (line) => {
   input.push(line);
 }).on('close', () => {
   // 구현
+  console.log(Solution(input));
+
+  function Solution(input) {
+    const N = Number(input[0]);
+
+    const dp = [0, 1, 2];
+    for (let i = 3; i <= N; i++) {
+      dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+    }
+
+    return dp[N];
+  }
 });
--- 동적계획법/test.js
+++ 동적계획법/test.js
@@ -1,22 +1,14 @@
 // const input = ['2'];
-const input = ['10'];
+const input = ['9'];
 
 console.log(Solution(input));
 
 function Solution(input) {
   const N = Number(input[0]);
 
-  const dp = [0, 0];
-  for (let i = 2; i <= N; i++) {
-    dp[i] = dp[i - 1] + 1;
-
-    if (i % 2 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-    }
-
-    if (i % 3 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-    }
+  const dp = [0, 1, 2];
+  for (let i = 3; i <= N; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
   }
 
   return dp[N];
