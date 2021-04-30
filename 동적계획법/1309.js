ece13e82bcb85b5df8a54bc6055fb7e07da4495d
MyeonghoonNam <codenmh0822@naver.com>
2021-04-30 18:47:04 +0900

    문제 해결

---
+++ 동적계획법/1309.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on('line', (line) => {
+  // 입력 관리
+  input.push(line);
+}).on('close', () => {
+  // 구현
+  console.log(Solution(input));
+
+  function Solution(input) {
+    const N = Number(input[0]);
+    const MOD = 9901;
+
+    const dp = Array.from(new Array(N + 1), () => new Array(3).fill(0));
+
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    dp[1][2] = 1;
+
+    for (let i = 2; i <= N; i++) {
+      dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+      dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+      dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+    }
+
+    const result = dp[N].reduce((arr, cur) => {
+      return arr + cur;
+    });
+
+    return result % MOD;
+  }
+});
--- 동적계획법/test.js
+++ 동적계획법/test.js
@@ -1,24 +1,26 @@
-const input = ['3', '26 40 83', '49 60 57', '13 89 99'];
+const input = ['4'];
 
 console.log(Solution(input));
 
 function Solution(input) {
-  const N = Number(input.shift());
-
-  const cost = Array.from(new Array(N + 1), () => new Array(3).fill(0));
-
-  for (let i = 1; i <= N; i++) {
-    cost[i] = input.shift().split(' ').map(Number);
-  }
+  const N = Number(input[0]);
+  const MOD = 9901;
 
   const dp = Array.from(new Array(N + 1), () => new Array(3).fill(0));
 
-  dp[0] = cost[0];
-  for (let i = 1; i <= N; i++) {
-    dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-    dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-    dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+  dp[1][0] = 1;
+  dp[1][1] = 1;
+  dp[1][2] = 1;
+
+  for (let i = 2; i <= N; i++) {
+    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+    dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
   }
 
-  return Math.min(...dp[N]);
+  const result = dp[N].reduce((arr, cur) => {
+    return arr + cur;
+  });
+
+  return result % MOD;
 }
