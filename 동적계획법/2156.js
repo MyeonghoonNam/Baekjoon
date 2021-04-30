0bb6e9a2db1b37177d5628b7f717d12cd220a203
MyeonghoonNam <codenmh0822@naver.com>
2021-04-30 20:11:50 +0900

    문제 복습

--- 동적계획법/2156.js
+++ 동적계획법/2156.js
@@ -1,32 +1,14 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const input = [];
-rl.on("line", line => {
-  input.push(parseInt(line));
-})
-  .on("close", () => {
-
-    const N = input[0];
-    input[0] = 0;
-    
-    const dp = [];
-
-    dp[0] = 0;
-    dp[1] = input[1];
-    
-    if(N > 1){
-      dp[2] = input[1] + input[2];
-    }
-
-    for(let i = 3; i <= N; i++){
-      dp[i] = Math.max(dp[i-1], Math.max(dp[i-3] + input[i] + input[i-1], dp[i-2] + input[i]));
-    }
-
-    console.log(dp[N]);
-    process.exit();
-  })
+rl.on('line', (line) => {
+  // 입력 관리
+  input.push(line);
+}).on('close', () => {
+  // 구현
+});
