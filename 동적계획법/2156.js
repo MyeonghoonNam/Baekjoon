de546358ed900ddfeb314cf783d465732b640b32
MyeonghoonNam <codenmh0822@naver.com>
2021-04-30 20:23:04 +0900

    문제 복습

--- 동적계획법/2156.js
+++ 동적계획법/2156.js
@@ -11,4 +11,27 @@ rl.on('line', (line) => {
   input.push(line);
 }).on('close', () => {
   // 구현
+  console.log(Solution(input));
+
+  function Solution(input) {
+    const N = Number(input.shift());
+    const arr = [0];
+    for (let i = 0; i < N; i++) {
+      const num = Number(input.shift());
+      arr.push(num);
+    }
+
+    const dp = [0];
+    dp[1] = arr[1];
+    dp[2] = dp[1] + arr[2];
+    for (let i = 3; i <= N; i++) {
+      dp[i] = Math.max(
+        dp[i - 1],
+        dp[i - 2] + arr[i],
+        dp[i - 3] + arr[i - 1] + arr[i]
+      );
+    }
+
+    return dp[N];
+  }
 });
--- 동적계획법/test.js
+++ 동적계획법/test.js
@@ -1,39 +1,25 @@
-const input = [
-  '2',
-  '5',
-  '50 10 100 20 40',
-  '30 50 70 10 60',
-  '7',
-  '10 30 10 50 100 20 40',
-  '20 40 30 50 60 20 80',
-];
+const input = ['6', '6', '10', '13', '9', '8', '1'];
 
-Solution(input);
+console.log(Solution(input));
 
 function Solution(input) {
-  const T = Number(input.shift());
-
-  for (let t = 0; t < T; t++) {
-    const N = Number(input.shift());
-    const map = Array.from(new Array(3), () => new Array(N + 1).fill(0));
-    const dp = Array.from(new Array(3), () => new Array(N + 1).fill(0));
-
-    for (let i = 1; i < 3; i++) {
-      const cost = input.shift().split(' ').map(Number);
-
-      for (let j = 1; j <= N; j++) {
-        map[i][j] = cost[j - 1];
-      }
-    }
-
-    dp[1][1] = map[1][1];
-    dp[2][1] = map[2][1];
-    for (let i = 2; i <= N; i++) {
-      dp[1][i] = Math.max(dp[2][i - 1], dp[2][i - 2]) + map[1][i];
-      dp[2][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + map[2][i];
-    }
-
-    const result = Math.max(dp[1][N], dp[2][N]);
-    console.log(result);
+  const N = Number(input.shift());
+  const arr = [0];
+  for (let i = 0; i < N; i++) {
+    const num = Number(input.shift());
+    arr.push(num);
   }
+
+  const dp = [0];
+  dp[1] = arr[1];
+  dp[2] = dp[1] + arr[2];
+  for (let i = 3; i <= N; i++) {
+    dp[i] = Math.max(
+      dp[i - 1],
+      dp[i - 2] + arr[i],
+      dp[i - 3] + arr[i - 1] + arr[i]
+    );
+  }
+
+  return dp[N];
 }
