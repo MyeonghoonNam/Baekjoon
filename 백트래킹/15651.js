f8a045ba4e77cde83035b8b2389383965a11de60
MyeongHoonNam <codenmh0822@naver.com>
2021-05-06 18:13:01 +0900

    문제 복습

--- 백트래킹/15651.js
+++ 백트래킹/15651.js
@@ -11,4 +11,22 @@ rl.on('line', (line) => {
   input.push(line);
 }).on('close', () => {
   // 구현
+  const [N, M] = input[0].split(' ').map(Number);
+  const arr = [];
+  let result = '';
+
+  Dfs(0);
+  console.log(result);
+
+  function Dfs(cnt) {
+    if (cnt === M) {
+      result += arr.join(' ') + '\n';
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      arr[cnt] = i;
+      Dfs(cnt + 1);
+    }
+  }
 });
--- 백트래킹/test.js
+++ 백트래킹/test.js
@@ -1,27 +1,22 @@
 // const input = ['3 1'];
-const input = ['4 2'];
-// const input = ['4 4'];
+// const input = ['4 2'];
+const input = ['3 3'];
 
 const [N, M] = input[0].split(' ').map(Number);
-const visited = new Array(N + 1).fill(false);
 const arr = [];
 let result = '';
 
-Dfs(1, 0);
+Dfs(0);
 console.log(result);
 
-function Dfs(idx, cnt) {
+function Dfs(cnt) {
   if (cnt === M) {
     result += arr.join(' ') + '\n';
     return;
   }
 
-  for (let i = idx; i <= N; i++) {
-    if (!visited[i]) {
-      visited[i] = true;
-      arr[cnt] = i;
-      Dfs(i + 1, cnt + 1);
-      visited[i] = false;
-    }
+  for (let i = 1; i <= N; i++) {
+    arr[cnt] = i;
+    Dfs(cnt + 1);
   }
 }
