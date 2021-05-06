6873a1540417ba667509748ad4fe0a2b8735efd2
MyeongHoonNam <codenmh0822@naver.com>
2021-05-06 18:07:02 +0900

    문제 복습

--- 백트래킹/15651.js
+++ 백트래킹/15651.js
@@ -1,36 +1,14 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('line', line => {
-  const input = line.split(' ').map(el => parseInt(el));
-  const N = input[0];
-  const M = input[1];
-
-  const output = [];
-  let result = '';
-
-  function dfs(cnt){
-    if(cnt === M){
-      result += `${output.join(' ')}\n`;
-      return;
-    }
-
-    for(let i = 0; i < N; i++){
-      output.push(i + 1);
-      dfs(output.length);
-      output.pop();
-    }
-  }
-
-  dfs(0);
-  console.log(result);
-  rl.close();
-})
-  .on('close', () => {
-    process.exit();
-  })
-
+const input = [];
+rl.on('line', (line) => {
+  // 입력 관리
+  input.push(line);
+}).on('close', () => {
+  // 구현
+});
