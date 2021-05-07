9eded08400856ae8fab0f2d8e8bcd0c3ff83bd3d
MyeongHoonNam <codenmh0822@naver.com>
2021-05-07 23:47:44 +0900

    문제 복습

--- 그래프/1697.js
+++ 그래프/1697.js
@@ -1,44 +1,44 @@
-const readline = require('readline');
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+  ? fs.readFileSync('/dev/stdin').toString()
+  : `5 17`
+).split('\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-const strToNumArr = (str) => str.split(' ').map(Number);
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line.trim());
-}).on('close', () => {
-  // 구현
-  const [N, K] = strToNumArr(input[0]);
-  const visited = new Array(100001).fill(false);
+const [N, K] = input().split(' ').map(Number);
+const visited = new Array(1000001).fill(false);
 
-  const queue = [[N, 0]];
+console.log(Solution());
 
-  while (queue.length) {
-    const [pos, count] = queue.shift();
+function Solution() {
+  const q = [];
+  q.push([N, 0]);
+  visited[N] = true;
 
-    if (visited[pos]) continue;
-
-    visited[pos] = true;
+  while (q.length > 0) {
+    const [pos, time] = q.shift();
 
     if (pos === K) {
-      console.log(count);
-      break;
+      return time;
     }
 
-    if (pos * 2 <= 100000) {
-      queue.push([pos * 2, count + 1]);
+    if (pos - 1 >= 0 && !visited[pos - 1]) {
+      visited[pos - 1] = true;
+      q.push([pos - 1, time + 1]);
     }
 
-    if (pos + 1 <= 100000) {
-      queue.push([pos + 1, count + 1]);
+    if (pos + 1 <= 100000 && !visited[pos + 1]) {
+      visited[pos + 1] = true;
+      q.push([pos + 1, time + 1]);
     }
 
-    if (pos - 1 >= 0) {
-      queue.push([pos - 1, count + 1]);
+    if (pos * 2 <= 100000 && !visited[pos * 2]) {
+      visited[pos * 2] = true;
+      q.push([pos * 2, time + 1]);
     }
   }
-});
+}
