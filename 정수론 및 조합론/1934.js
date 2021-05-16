66c8e4e06d76991554c9f3459930233eebc05109
MyeongHoonNam <codenmh0822@naver.com>
2021-05-16 22:06:49 +0900

    문제 복습

--- 정수론 및 조합론/1934.js
+++ 정수론 및 조합론/1934.js
@@ -1,40 +1,41 @@
-// 유클리드 호제법을 활용한 최대공약수와 최소공배수 구하기
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `3
+1 45000
+6 10
+13 17`
+).split('\n');
 
-const readline = require('readline');
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+Solution();
 
-const input = [];
-rl.on('line', (line) => {
-  input.push(line.split(' ').map((el) => parseInt(el)));
-}).on('close', () => {
-  const N = input[0][0];
-  const numbers = input.slice(1);
+function Solution() {
+  const T = Number(input());
 
-  let result = '';
-  for (let i = 0; i < N; i++) {
-    result +=
-      LCM(numbers[i][0] * numbers[i][1], GCD(numbers[i][0], numbers[i][1])) +
-      '\n';
+  for (let i = 0; i < T; i++) {
+    const [num1, num2] = input().split(' ').map(Number);
+
+    console.log(LCM(num1, num2));
+  }
+}
+
+function GCD(a, b) {
+  while (b !== 0) {
+    const r = a % b;
+
+    a = b;
+    b = r;
   }
 
-  console.log(result);
-  process.exit();
+  return a;
+}
 
-  // 최대 공약수
-  function GCD(number1, number2) {
-    if (number2 === 0) {
-      return number1;
-    } else {
-      return GCD(number2, number1 % number2);
-    }
-  }
-
-  // 최소 공배수
-  function LCM(numbersMultiple, GCD) {
-    return numbersMultiple / GCD;
-  }
-});
+function LCM(a, b) {
+  return (a * b) / GCD(a, b);
+}
