e728b2d15b0a09d2cabad51a4a2d77da1df13d71
MyeongHoonNam <codenmh0822@naver.com>
2021-05-18 13:18:04 +0900

    문제 복습

--- 정수론 및 조합론/1676.js
+++ 정수론 및 조합론/1676.js
@@ -1,20 +1,25 @@
-const readline = require('readline');
+// 팩토리얼에서는 5의 승수를 구하면 간단히 0의 개수를 구할 수 있다.
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux' ? fs.readFileSync('/dev/stdin').toString() : `10`
+).split('\n');
 
-rl.on('line', (line) => {
-  let number = parseInt(line);
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+console.log(Solution());
+
+function Solution() {
+  let N = Number(input());
   let count = 0;
 
-  while (number >= 5) {
-    count += parseInt(number / 5);
-    number = parseInt(number / 5);
+  while (N >= 5) {
+    count += N / 5;
+    N = N / 5;
   }
 
-  console.log(count);
-}).on('close', () => {
-  process.exit();
-});
+  return count;
+}
