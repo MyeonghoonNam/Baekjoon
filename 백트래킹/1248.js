97e22c87731e670927caec51b099cfd4058fc237
MyeongHoonNam <codenmh0822@naver.com>
2021-05-25 16:23:10 +0900

    문제 복습

--- 백트래킹/1248.js
+++ 백트래킹/1248.js
@@ -1,63 +1,3 @@
-// const fs = require('fs');
-// const stdin = (
-//   process.platform === 'linux'
-//     ? fs.readFileSync('/dev/stdin').toString()
-//     : `4
-// -+0++++--+`
-// ).split('\n');
-
-// const input = (() => {
-//   let line = 0;
-//   return () => stdin[line++];
-// })();
-
-// Solution();
-
-// function Solution() {
-//   const N = Number(input());
-//   const operator = Array.from(new Array(N), () => new Array());
-//   const selected = new Array(10).fill(0);
-
-//   const inputArr = input();
-//   for (let i = 0; i < N; i++) {
-//     for (let j = i; j < N; j++) {
-//       operator[i][j] = inputArr[j];
-//     }
-//   }
-
-//   const Check = (idx) => {
-//     let sum = 0;
-
-//     for (let i = idx; i >= 0; i--) {
-//       sum += selected[i];
-
-//       if (operator[i][idx] === '+' && sum <= 0) return false;
-//       if (operator[i][idx] === '-' && sum >= 0) return false;
-//       if (operator[i][idx] === '0' && sum !== 0) return false;
-//     }
-
-//     return true;
-//   };
-
-//   const Dfs = (cnt) => {
-//     if (cnt === N) {
-//       for (let i = 0; i < cnt; i++) {
-//         process.stdout.write(selected[i] + ' ');
-//       }
-
-//       return;
-//     }
-
-//     for (let i = -10; i <= 10; i++) {
-//       selected[cnt] = i;
-//       if (Check(cnt)) {
-//         Dfs(cnt + 1);
-//       }
-//     }
-//   };
-
-//   Dfs(0);
-// }
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -82,7 +22,6 @@ rl.on('line', (line) => {
     }
   }
 
-  const visited = [];
   const visit = [];
 
   dfs(0);
@@ -93,19 +32,12 @@ rl.on('line', (line) => {
       process.exit();
     }
     for (let i = 0; i < 21; i++) {
-      //if(visited[i]) continue;
-      //visited[i] = true;
       visit.push(i - 10);
 
-      const flag = solution();
-
-      if (flag === true) {
-        //console.log(flag);
-        //console.log(visit);
+      if (solution()) {
         dfs(cnt + 1);
       }
       visit.pop();
-      //visited[i] = false;
     }
   }
 
