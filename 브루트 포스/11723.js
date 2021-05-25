c868317c6528b241c96edfce9fca54ac53a68e0c
MyeongHoonNam <codenmh0822@naver.com>
2021-05-25 20:08:59 +0900

    문제 보류

---
+++ 브루트 포스/11723.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `26
+add 1
+add 2
+check 1
+check 2
+check 3
+remove 2
+check 1
+check 2
+toggle 3
+check 1
+check 2
+check 3
+check 4
+all
+check 10
+check 20
+toggle 10
+remove 20
+check 10
+check 20
+empty
+check 1
+toggle 1
+check 1
+toggle 1
+check 1`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+Solution();
+
+function Solution() {
+  const M = Number(input());
+  let BIT = 0;
+
+  for (let i = 0; i < M; i++) {
+    const [cmd, x] = input().split(' ');
+
+    switch (cmd) {
+      case 'add':
+        BIT |= 1 << x;
+        break;
+      case 'remove':
+        BIT &= ~(1 << x);
+        break;
+      case 'check':
+        if (BIT & (1 << x)) console.log(1);
+        else console.log(0);
+        break;
+      case 'toggle':
+        BIT ^= 1 << x;
+        break;
+      case 'all':
+        BIT = (1 << 21) - 1;
+        break;
+      case 'empty':
+        BIT = 0;
+        break;
+    }
+  }
+}
