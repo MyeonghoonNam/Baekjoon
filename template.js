57542880e09678385bd80ed968f9e126d9fc3e15
MyeongHoonNam <codenmh0822@naver.com>
2021-06-01 16:18:05 +0900

    템플릿 업데이트

--- template.js
+++ template.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 const stdin = (
   process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `입력관리 !`
+    : `입력값 입력`
 ).split('\n');
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
+
+console.log(Solution());
+
+function Solution() {}
