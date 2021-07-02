76c3e6272a39e0e5cb5eec7df84ac651825f1ce8
MyeonghoonNam <codenmh0822@naver.com>
2021-07-02 16:29:17 +0900

    문제 복습 - 성능 개선

--- 분할정복/10815.js
+++ 분할정복/10815.js
@@ -18,6 +18,7 @@ const input = (() => {
 
 console.log(Solution());
 
+// 해시 탐색
 function Solution() {
   const N = Number(input());
   const cardNum = input().split(' ').map(Number);
@@ -27,28 +28,64 @@ function Solution() {
 
   const result = [];
 
-  cardNum.sort((a, b) => a - b);
+  const map = new Map();
 
-  const binarySearch = (num) => {
-    let low = 0;
-    let high = cardNum.length - 1;
+  for (let i = 0; i < N; i++) {
+    const num = cardNum[i];
 
-    while (low <= high) {
-      let mid = Math.floor((low + high) / 2);
-
-      if (cardNum[mid] < num) low = mid + 1;
-      else if (cardNum[mid] > num) high = mid - 1;
-      else return 1;
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
     }
+  }
 
-    return 0;
-  };
+  for (let i = 0; i < M; i++) {
+    const num = findNum[i];
 
-  findNum.forEach((num) => {
-    const include = binarySearch(num);
-
-    result.push(include);
-  });
+    if (map.has(num)) {
+      result.push(1);
+    } else {
+      result.push(0);
+    }
+  }
 
   return result.join(' ');
 }
+
+// 이진탐색
+
+// function Solution() {
+//   const N = Number(input());
+//   const cardNum = input().split(' ').map(Number);
+
+//   const M = Number(input());
+//   const findNum = input().split(' ').map(Number);
+
+//   const result = [];
+
+//   cardNum.sort((a, b) => a - b);
+
+//   const binarySearch = (num) => {
+//     let low = 0;
+//     let high = cardNum.length - 1;
+
+//     while (low <= high) {
+//       let mid = Math.floor((low + high) / 2);
+
+//       if (cardNum[mid] < num) low = mid + 1;
+//       else if (cardNum[mid] > num) high = mid - 1;
+//       else return 1;
+//     }
+
+//     return 0;
+//   };
+
+//   findNum.forEach((num) => {
+//     const include = binarySearch(num);
+
+//     result.push(include);
+//   });
+
+//   return result.join(' ');
+// }
