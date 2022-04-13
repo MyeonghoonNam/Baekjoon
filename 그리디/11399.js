ccfb1ec04592834a25db62c026dc00b6a93afc9c
MyeongHoonNam <codenmh0822@naver.com>
2022-04-13 21:49:58 +0900

    문제 복습 - ATM

--- 그리디/11399.js
+++ 그리디/11399.js
@@ -1,32 +1,31 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `5
 3 1 4 3 2`
-).split('\n');
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-console.log(Solution());
-
-function Solution() {
+const solution = () => {
   const N = Number(input());
-  const withdrawal = input().split(' ').map(Number);
+  const times = input().split(" ").map(Number);
 
-  withdrawal.sort((a, b) => a - b);
+  times.sort((a, b) => a - b);
 
-  let sum = 0;
+  let result = 0;
+
   for (let i = 0; i < N; i++) {
     for (let j = 0; j <= i; j++) {
-      sum += withdrawal[j];
+      result += times[j];
     }
   }
 
-  return sum;
-}
+  return result;
+};
+
+console.log(solution());
