3e8552326621fe2f88e304b93fb2c1eddd3dc86e
MyeongHoonNam <codenmh0822@naver.com>
2022-04-19 17:02:35 +0900

    문제 복습 - 유기농 배추

--- 그래프/1012.js
+++ 그래프/1012.js
@@ -1,9 +1,7 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `1
 5 3 6
 0 2
@@ -12,51 +10,47 @@ const stdin = (
 3 2
 4 2
 4 0`
-).split('\n');
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-Solution();
-
-function Solution() {
+const solution = () => {
   let T = Number(input());
+  const result = [];
 
   while (T--) {
-    const [M, N, K] = input().split(' ').map(Number);
+    const [M, N, K] = input().split(" ").map(Number);
     const map = Array.from(new Array(N), () => new Array(M).fill(0));
     const visited = Array.from(new Array(N), () => new Array(M).fill(false));
-    let landSize = 0;
+    let count = 0;
 
     for (let i = 0; i < K; i++) {
-      const [x, y] = input().split(' ').map(Number);
-
+      const [x, y] = input().split(" ").map(Number);
       map[y][x] = 1;
     }
 
-    const dy = [-1, 1, 0, 0];
     const dx = [0, 0, -1, 1];
+    const dy = [-1, 1, 0, 0];
 
     const dfs = (y, x) => {
-      if (visited[y][x]) return;
-
       visited[y][x] = true;
 
       for (let i = 0; i < 4; i++) {
-        const ny = y + dy[i];
         const nx = x + dx[i];
+        const ny = y + dy[i];
 
-        if (checkRange(ny, nx) && map[ny][nx] === 1) {
-          if (!visited[ny][nx]) {
-            dfs(ny, nx);
-          }
+        if (!checkMapRange(ny, nx)) continue;
+
+        if (map[ny][nx] === 1 && !visited[ny][nx]) {
+          dfs(ny, nx);
         }
       }
     };
 
-    const checkRange = (y, x) => {
+    const checkMapRange = (y, x) => {
       if (x >= 0 && x < M && y >= 0 && y < N) return true;
       else return false;
     };
@@ -65,11 +59,15 @@ function Solution() {
       for (let j = 0; j < M; j++) {
         if (!visited[i][j] && map[i][j] === 1) {
           dfs(i, j);
-          landSize++;
+          count += 1;
         }
       }
     }
 
-    console.log(landSize);
+    result.push(count);
   }
-}
+
+  return result.join("\n");
+};
+
+console.log(solution());
