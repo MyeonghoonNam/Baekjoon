14039eaa9a2b4a7f5ea793a8fd5c6e2182bd5d83
MyeongHoonNam <codenmh0822@naver.com>
2022-05-22 17:08:25 +0900

    문제 복습 - 가장 긴 증가하는 부분 수열 4

--- 동적계획법/14002.js
+++ 동적계획법/14002.js
@@ -1,49 +1,42 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `6
+10 20 10 30 20 50`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+const solution = () => {
+  const N = Number(input());
+  const numbers = input().split(" ").map(Number);
+  const DP = [];
 
-  function Solution(input) {
-    const N = Number(input.shift());
-    const arr = input.shift().split(' ').map(Number);
+  for (let i = 0; i < N; i++) {
+    DP[i] = 1;
 
-    const dp = [];
-    for (let i = 0; i < N; i++) {
-      dp[i] = 1;
-
-      for (let j = 0; j < i; j++) {
-        if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-          dp[i] = dp[j] + 1;
-        }
+    for (let j = 0; j < i; j++) {
+      if (numbers[i] > numbers[j] && DP[i] < DP[j] + 1) {
+        DP[i] = DP[j] + 1;
       }
     }
-
-    const result = [];
-    let max = Math.max(...dp);
-    result[0] = max;
-
-    let path = [];
-    for (let i = N - 1; i >= 0; i--) {
-      if (dp[i] === max) {
-        path.push(arr[i]);
-        max--;
-      }
-    }
-
-    result[1] = path.reverse();
-
-    const print = `${result[0]}\n${result[1].join(' ')}`;
-
-    return print;
   }
-});
+
+  const result = [];
+  let max_length = Math.max(...DP);
+
+  for (let i = N - 1; i >= 0; i--) {
+    if (DP[i] === max_length) {
+      result.push(numbers[i]);
+      max_length--;
+    }
+  }
+
+  return `${result.length}\n${result.reverse().join(" ")}`;
+};
+
+console.log(solution());
