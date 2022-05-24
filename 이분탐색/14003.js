bcdcc945fcefa19de05c3bf23cd068203675fd1f
MyeongHoonNam <codenmh0822@naver.com>
2022-05-24 16:33:28 +0900

    문제 해결 - 가장 긴 증가하는 부분 수열 5

--- 이분탐색/14003.js
+++ 이분탐색/14003.js
@@ -1,52 +1,62 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `6
-10 20 10 30 20 50`
-).split('\n');
+10 20 10 30 15 50`
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-Solution();
-
-function Solution() {
+const solution = () => {
   const N = Number(input());
-  const numbers = input().split(' ').map(Number);
-  const list = [numbers[0]];
-
-  const binarySearch = (target) => {
-    let low = 0;
-    let high = list.length;
-
-    while (low < high) {
-      let mid = Math.floor((low + high) / 2);
-
-      if (list[mid] < target) {
-        low = mid + 1;
-      } else {
-        high = mid;
-      }
-    }
-
-    if (list[high] >= target) return high;
-  };
+  const numbers = input().split(" ").map(Number);
+  const lis = [numbers[0]];
+  const lis_index = [0];
 
   for (let i = 1; i < N; i++) {
-    const num = numbers[i];
-    if (list[list.length - 1] < num) {
-      list.push(num);
+    if (lis[lis.length - 1] < numbers[i]) {
+      lis.push(numbers[i]);
+      lis_index[i] = lis.length - 1;
     } else {
-      const idx = binarySearch(num);
-      list[idx] = num;
+      const index = lowerBound(numbers[i], lis);
+
+      lis[index] = numbers[i];
+      lis_index[i] = index;
     }
   }
 
-  console.log(list.length);
-  console.log(list.join(' '));
-}
+  const result = [];
+  let find_index = lis.length - 1;
+
+  for (let i = N - 1; i >= 0; i--) {
+    if (lis_index[i] === find_index) {
+      find_index--;
+      result.push(numbers[i]);
+    }
+  }
+
+  return `${result.length}\n${result.reverse().join(" ")}`;
+};
+
+const lowerBound = (target, lis) => {
+  let low = 0;
+  let high = lis.length - 1;
+
+  while (low < high) {
+    const mid = parseInt((low + high) / 2);
+
+    if (lis[mid] >= target) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return low;
+};
+
+console.log(solution());
