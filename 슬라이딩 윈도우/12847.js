a18ee1be33526c0480ac4976f0493e693ca55900
MyeongHoonNam <codenmh0822@naver.com>
2022-06-29 21:25:56 +0900

    문제 해결 - 꿀 아르바이트 (슬라이딩 윈도우)

--- 슬라이딩 윈도우/12847.js
+++ 슬라이딩 윈도우/12847.js
@@ -11,20 +11,42 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 누적 합 알고리즘
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const arr = input().split(" ").map(Number);
+//   const prefix_sum = [0];
+
+//   let sum = 0;
+//   for (let i = 0; i < N; i++) {
+//     sum += arr[i];
+//     prefix_sum.push(sum);
+//   }
+
+//   let result = 0;
+//   for (let i = M; i <= N; i++) {
+//     let sum = prefix_sum[i] - prefix_sum[i - M];
+//     result = Math.max(result, sum);
+//   }
+
+//   return result;
+// };
+
+// 슬라이딩 윈도우 알고리즘
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
   const arr = input().split(" ").map(Number);
-  const prefix_sum = [0];
-
+  let result = 0;
   let sum = 0;
-  for (let i = 0; i < N; i++) {
+
+  for (let i = 0; i < M; i++) {
     sum += arr[i];
-    prefix_sum.push(sum);
   }
 
-  let result = 0;
-  for (let i = M; i <= N; i++) {
-    let sum = prefix_sum[i] - prefix_sum[i - M];
+  result = sum;
+
+  for (let i = M; i < N; i++) {
+    sum += arr[i] - arr[i - M];
     result = Math.max(result, sum);
   }
 
