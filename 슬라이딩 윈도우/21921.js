22e3ea3f2e2494375a27ac5d13d5e945b6fa7da3
MyeongHoonNam <codenmh0822@naver.com>
2022-07-01 18:58:46 +0900

    문제 해결 - 블로그

--- 슬라이딩 윈도우/21921.js
+++ 슬라이딩 윈도우/21921.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `5 2
-1 4 2 5 1`
+    : `5 3
+0 0 0 0 0`
 ).split("\n");
 
 const input = (() => {
@@ -13,14 +13,11 @@ const input = (() => {
 
 const solution = () => {
   const [N, X] = input().split(" ").map(Number);
-  const data = input().split(" ").map(Number);
-  const arr = new Array(250000);
+  const arr = input().split(" ").map(Number);
   let sum = 0;
 
-  for (let i = 0; i < N; i++) {
-    const day = data[i];
-    arr[i + 1] = day;
-    sum += arr[i + 1];
+  for (let i = 0; i < X; i++) {
+    sum += arr[i];
   }
 
   let result = sum;
