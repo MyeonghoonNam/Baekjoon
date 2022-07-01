141cbf644651e799ccc30994f9e686d2f851bbfb
MyeongHoonNam <codenmh0822@naver.com>
2022-07-01 19:07:16 +0900

    문제 해결 - 블로그 (누적합)

--- 슬라이딩 윈도우/21921.js
+++ 슬라이딩 윈도우/21921.js
@@ -11,26 +11,61 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 슬라이딩 윈도우
+// const solution = () => {
+//   const [N, X] = input().split(" ").map(Number);
+//   const arr = input().split(" ").map(Number);
+//   let sum = 0;
+
+//   for (let i = 0; i < X; i++) {
+//     sum += arr[i];
+//   }
+
+//   let result = sum;
+//   let count = 1;
+
+//   for (let i = X; i < N; i++) {
+//     sum += arr[i] - arr[i - X];
+
+//     if (sum >= result) {
+//       if (sum === result) {
+//         count++;
+//       } else {
+//         result = sum;
+//         count = 1;
+//       }
+//     }
+//   }
+
+//   if (result === 0) {
+//     return "SAD";
+//   } else {
+//     return `${result}\n${count}`;
+//   }
+// };
+
+// 누적합
 const solution = () => {
   const [N, X] = input().split(" ").map(Number);
   const arr = input().split(" ").map(Number);
-  let sum = 0;
+  const prefix_sum = [0];
 
-  for (let i = 0; i < X; i++) {
+  let sum = 0;
+  for (let i = 0; i < N; i++) {
     sum += arr[i];
+    prefix_sum.push(sum);
   }
 
-  let result = sum;
+  let result = 0;
   let count = 1;
+  for (let i = X; i <= N; i++) {
+    let value = prefix_sum[i] - prefix_sum[i - X];
 
-  for (let i = X; i < N; i++) {
-    sum += arr[i] - arr[i - X];
-
-    if (sum >= result) {
-      if (sum === result) {
+    if (value >= result) {
+      if (value === result) {
         count++;
       } else {
-        result = sum;
+        result = value;
         count = 1;
       }
     }
