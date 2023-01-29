86a03490bcace24f63e0574fd2284dca5e984fa6
MyeongHoonNam <codenmh0822@naver.com>
2023-01-29 19:57:10 +0900

    문제 복습

--- 동적계획법/11052.js
+++ 동적계획법/11052.js
@@ -1,32 +1,61 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `4
+1 5 6 7`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+const solution = () => {
+  const N = Number(input());
+  const cards = input().split(" ").map(Number);
+  const DP = new Array(N + 1).fill(0);
 
-  function Solution(input) {
-    const N = Number(input.shift());
-    const cards = input.shift().split(' ').map(Number);
-    cards.unshift(0);
-
-    const dp = [0, cards[1]];
-    for (let i = 2; i <= N; i++) {
-      dp[i] = 0;
-
-      for (let j = 1; j <= i; j++) {
-        dp[i] = Math.max(dp[i], dp[i - j] + cards[j]);
-      }
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= i; j++) {
+      DP[i] = Math.max(DP[i], DP[i - j] + cards[j - 1]);
     }
-
-    return dp[N];
   }
-});
+
+  return DP[N];
+};
+
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// const input = [];
+// rl.on('line', (line) => {
+//   // 입력 관리
+//   input.push(line);
+// }).on('close', () => {
+//   // 구현
+//   console.log(Solution(input));
+
+//   function Solution(input) {
+//     const N = Number(input.shift());
+//     const cards = input.shift().split(' ').map(Number);
+//     cards.unshift(0);
+
+//     const dp = [0, cards[1]];
+//     for (let i = 2; i <= N; i++) {
+//       dp[i] = 0;
+
+//       for (let j = 1; j <= i; j++) {
+//         dp[i] = Math.max(dp[i], dp[i - j] + cards[j]);
+//       }
+//     }
+
+//     return dp[N];
+//   }
+// });
