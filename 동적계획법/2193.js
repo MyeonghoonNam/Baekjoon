5a0bb0bf41e8a4e783a6493bb9e170ba1332b417
MyeongHoonNam <codenmh0822@naver.com>
2023-01-30 20:01:26 +0900

    문제 2차 해결

--- 동적계획법/2193.js
+++ 동적계획법/2193.js
@@ -1,31 +1,57 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `3`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+const solution = () => {
+  const N = Number(input());
+  const DP = [];
 
-  function Solution(input) {
-    const N = BigInt(input[0]);
+  DP[0] = 0n;
+  DP[1] = 1n;
 
-    const dp = [];
-
-    dp[0] = 0n;
-    dp[1] = 1n;
-    dp[2] = 1n;
-
-    for (let i = 3; i <= N; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2];
-    }
-
-    return dp[N].toString();
+  for (let i = 2; i <= N; i++) {
+    DP[i] = DP[i - 1] + DP[i - 2];
   }
-});
+
+  return DP[N].toString();
+};
+
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// const input = [];
+// rl.on('line', (line) => {
+//   // 입력 관리
+//   input.push(line);
+// }).on('close', () => {
+//   // 구현
+//   console.log(Solution(input));
+
+//   function Solution(input) {
+//     const N = BigInt(input[0]);
+
+//     const dp = [];
+
+//     dp[0] = 0n;
+//     dp[1] = 1n;
+//     dp[2] = 1n;
+
+//     for (let i = 3; i <= N; i++) {
+//       dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+
+//     return dp[N].toString();
+//   }
+// });
