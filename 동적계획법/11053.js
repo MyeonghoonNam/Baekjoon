fdaf534d657392521010743b6c8cd7495d3ff82c
MyeongHoonNam <codenmh0822@naver.com>
2023-01-30 20:16:39 +0900

    문제 2차 해결

--- 동적계획법/11053.js
+++ 동적계획법/11053.js
@@ -13,7 +13,7 @@ const input = (() => {
 
 const solution = () => {
   const N = Number(input());
-  const numbers = input().split(" ").map(Number);
+  const arr = input().split(" ").map(Number);
   const DP = [];
   let result = 0;
 
@@ -21,15 +21,51 @@ const solution = () => {
     DP[i] = 1;
 
     for (let j = 0; j < i; j++) {
-      if (numbers[i] > numbers[j] && DP[i] < DP[j] + 1) {
+      if (arr[i] > arr[j] && DP[i] < DP[j] + 1) {
         DP[i] = DP[j] + 1;
       }
     }
 
-    result = result < DP[i] ? DP[i] : result;
+    result = Math.max(result, DP[i]);
   }
 
   return result;
 };
 
 console.log(solution());
+
+// const fs = require("fs");
+// const stdin = (
+//   process.platform === "linux"
+//     ? fs.readFileSync("/dev/stdin").toString()
+//     : `6
+// 10 20 10 30 20 50`
+// ).split("\n");
+
+// const input = (() => {
+//   let line = 0;
+//   return () => stdin[line++];
+// })();
+
+// const solution = () => {
+//   const N = Number(input());
+//   const numbers = input().split(" ").map(Number);
+//   const DP = [];
+//   let result = 0;
+
+//   for (let i = 0; i < N; i++) {
+//     DP[i] = 1;
+
+//     for (let j = 0; j < i; j++) {
+//       if (numbers[i] > numbers[j] && DP[i] < DP[j] + 1) {
+//         DP[i] = DP[j] + 1;
+//       }
+//     }
+
+//     result = result < DP[i] ? DP[i] : result;
+//   }
+
+//   return result;
+// };
+
+// console.log(solution());
