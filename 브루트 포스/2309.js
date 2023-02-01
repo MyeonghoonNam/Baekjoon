c55fb948dbf3cef091e24fb49c56228405edbfdb
MyeongHoonNam <codenmh0822@naver.com>
2023-02-01 20:09:28 +0900

    문제 2차 해결

--- 브루트 포스/2309.js
+++ 브루트 포스/2309.js
@@ -1,38 +1,90 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `20
+7
+23
+19
+10
+15
+25
+8
+13`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(Number(line));
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+// 문제 2차 해결
+const solution = () => {
+  const people = [];
+  const result = [];
+  let sum = 0;
 
-  function Solution(input) {
-    input.sort((a, b) => a - b);
+  for (let i = 0; i < 9; i++) {
+    const height = Number(input());
 
-    const sum = input.reduce((acc, cur) => {
-      return acc + cur;
-    });
+    people.push(height);
+    sum += height;
+  }
 
-    let result = '';
-    for (let i = 0; i < input.length; i++) {
-      for (let j = i + 1; j < input.length; j++) {
-        if (sum - input[i] - input[j] === 100) {
-          for (let k = 0; k < input.length; k++) {
-            if (k === i || k === j) continue;
+  people.sort((a, b) => a - b);
 
-            result += input[k] + '\n';
-          }
+  for (let i = 0; i < 9; i++) {
+    for (let j = i + 1; j < 9; j++) {
+      if (sum - (people[i] + people[j]) !== 100) continue;
 
-          return result;
-        }
+      for (let k = 0; k < 9; k++) {
+        if (k === i || k === j) continue;
+
+        result.push(people[k]);
       }
+
+      return result.join("\n");
     }
   }
-});
+};
+
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// const input = [];
+// rl.on('line', (line) => {
+//   // 입력 관리
+//   input.push(Number(line));
+// }).on('close', () => {
+//   // 구현
+//   console.log(Solution(input));
+
+//   function Solution(input) {
+//     input.sort((a, b) => a - b);
+
+//     const sum = input.reduce((acc, cur) => {
+//       return acc + cur;
+//     });
+
+//     let result = '';
+//     for (let i = 0; i < input.length; i++) {
+//       for (let j = i + 1; j < input.length; j++) {
+//         if (sum - input[i] - input[j] === 100) {
+//           for (let k = 0; k < input.length; k++) {
+//             if (k === i || k === j) continue;
+
+//             result += input[k] + '\n';
+//           }
+
+//           return result;
+//         }
+//       }
+//     }
+//   }
+// });
