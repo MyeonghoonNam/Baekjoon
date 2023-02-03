9164395b68448e20a08a77be791956974f545609
MyeongHoonNam <codenmh0822@naver.com>
2023-02-03 19:27:08 +0900

    문제 해결

--- 브루트 포스/2231.js
+++ 브루트 포스/2231.js
@@ -1,42 +1,85 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `216`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-rl.on('line', line => {
-  const C = parseInt(line);
+const solution = () => {
+  const N = Number(input());
 
   let min = 0;
   let num = 1;
-  while(num <= 1000000){
 
+  while (num <= 1000000) {
     let sum = num;
     let temp = num;
-    while(parseInt(temp / 10) != 0){
+
+    while (parseInt(temp / 10) !== 0) {
       sum += temp % 10;
       temp = parseInt(temp / 10);
     }
 
     sum += temp % 10;
 
-    if(min == 0 && sum === C){
+    if (min == 0 && sum === N) {
       min = num;
-    } else if(min > num && sum === C){
+    } else if (min > num && sum === N) {
       min = num;
     }
-  
+
     num++;
   }
 
-  if(min === 0){
-    console.log(min);
-  } else{
-    console.log(min);
-  }
+  return min;
+};
 
-})
-  .on('close', () => {
-    process.exit();
-  })
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input:process.stdin,
+//   output:process.stdout
+// });
+
+// rl.on('line', line => {
+//   const C = parseInt(line);
+
+//   let min = 0;
+//   let num = 1;
+//   while(num <= 1000000){
+
+//     let sum = num;
+//     let temp = num;
+//     while(parseInt(temp / 10) != 0){
+//       sum += temp % 10;
+//       temp = parseInt(temp / 10);
+//     }
+
+//     sum += temp % 10;
+
+//     if(min == 0 && sum === C){
+//       min = num;
+//     } else if(min > num && sum === C){
+//       min = num;
+//     }
+
+//     num++;
+//   }
+
+//   if(min === 0){
+//     console.log(min);
+//   } else{
+//     console.log(min);
+//   }
+
+// })
+//   .on('close', () => {
+//     process.exit();
+//   })
