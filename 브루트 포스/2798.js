e0f4f20ca497328b278e008d75c73908f702751f
MyeongHoonNam <codenmh0822@naver.com>
2023-02-03 19:01:36 +0900

    문제 해결

--- 브루트 포스/2798.js
+++ 브루트 포스/2798.js
@@ -1,30 +1,26 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : `10 500
-93 181 245 214 315 36 185 138 216 295`
-).split('\n');
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5 21
+5 6 7 8 9`
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-console.log(Solution());
-
-function Solution() {
-  const [N, M] = input().split(' ').map(Number);
-  const numbers = input().split(' ').map(Number);
-  const choiceNumbers = [];
-  const choiced = new Array(numbers.length).fill(false);
+const solution = () => {
+  const [N, M] = input().split(" ").map(Number);
+  const numbers = input().split(" ").map(Number);
+  const selectedNumbers = [];
+  const selectedIndex = new Array(numbers.length).fill(false);
   let result = 0;
 
   const dfs = (idx, cnt) => {
     if (cnt === 3) {
-      const sum = choiceNumbers.reduce((acc, cur) => {
+      const sum = selectedNumbers.reduce((acc, cur) => {
         return acc + cur;
       });
 
@@ -34,11 +30,11 @@ function Solution() {
     }
 
     for (let i = idx; i < N; i++) {
-      if (!choiced[i]) {
-        choiced[i] = true;
-        choiceNumbers[cnt] = numbers[i];
+      if (!selectedIndex[i]) {
+        selectedIndex[i] = true;
+        selectedNumbers[cnt] = numbers[i];
         dfs(i, cnt + 1);
-        choiced[i] = false;
+        selectedIndex[i] = false;
       }
     }
   };
@@ -46,4 +42,56 @@ function Solution() {
   dfs(0, 0);
 
   return result;
-}
+};
+
+console.log(solution());
+
+// 'use strict';
+
+// const fs = require('fs');
+// const stdin = (
+//   process.platform === 'linux'
+//     ? fs.readFileSync('/dev/stdin').toString()
+//     : `10 500
+// 93 181 245 214 315 36 185 138 216 295`
+// ).split('\n');
+
+// const input = (() => {
+//   let line = 0;
+//   return () => stdin[line++];
+// })();
+
+// console.log(Solution());
+
+// function Solution() {
+//   const [N, M] = input().split(' ').map(Number);
+//   const numbers = input().split(' ').map(Number);
+//   const choiceNumbers = [];
+//   const choiced = new Array(numbers.length).fill(false);
+//   let result = 0;
+
+//   const dfs = (idx, cnt) => {
+//     if (cnt === 3) {
+//       const sum = choiceNumbers.reduce((acc, cur) => {
+//         return acc + cur;
+//       });
+
+//       if (sum <= M && sum > result) result = sum;
+
+//       return;
+//     }
+
+//     for (let i = idx; i < N; i++) {
+//       if (!choiced[i]) {
+//         choiced[i] = true;
+//         choiceNumbers[cnt] = numbers[i];
+//         dfs(i, cnt + 1);
+//         choiced[i] = false;
+//       }
+//     }
+//   };
+
+//   dfs(0, 0);
+
+//   return result;
+// }
