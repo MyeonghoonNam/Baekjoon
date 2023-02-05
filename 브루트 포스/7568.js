612dd42b91072b7b03c54d3566fa9a6443255c45
MyeongHoonNam <codenmh0822@naver.com>
2023-02-05 17:29:59 +0900

    문제 해결

--- 브루트 포스/7568.js
+++ 브루트 포스/7568.js
@@ -1,36 +1,84 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+55 185
+58 183
+88 186
+60 175
+46 155`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', line => {
-  input.push(line.split(' ').map(el => parseInt(el)));
-})
-  .on('close', () => {
+const solution = () => {
+  const N = Number(input());
+  const sizeTable = [];
+  const result = [];
 
-    const N = input[0];
-    const bodyInfo = input.slice(1);
+  for (let i = 0; i < N; i++) {
+    sizeTable.push(input().split(" ").map(Number));
+  }
 
-    const scoreList = [];
+  for (let i = 0; i < N; i++) {
+    let score = 0;
 
-    for(let i = 0; i < N; i++){
+    for (let j = 0; j < N; j++) {
+      if (i === j) continue;
 
-      let score = 0;
-      for(let j = 0; j < N; j++){
-        if(i === j) continue;
-
-        if(bodyInfo[i][0] < bodyInfo[j][0] && bodyInfo[i][1] < bodyInfo[j][1]){
-          score++;
-        }
+      if (
+        sizeTable[i][0] < sizeTable[j][0] &&
+        sizeTable[i][1] < sizeTable[j][1]
+      ) {
+        score++;
       }
-
-      scoreList.push(score + 1);
     }
 
-    console.log(scoreList.join(' '));
+    result.push(score + 1);
+  }
 
-    process.exit();
-  })
+  return result.join(" ");
+};
+
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input:process.stdin,
+//   output:process.stdout
+// });
+
+// const input = [];
+// rl.on('line', line => {
+//   input.push(line.split(' ').map(el => parseInt(el)));
+// })
+//   .on('close', () => {
+
+//     const N = input[0];
+//     const bodyInfo = input.slice(1);
+
+//     const scoreList = [];
+
+//     for(let i = 0; i < N; i++){
+
+//       let score = 0;
+//       for(let j = 0; j < N; j++){
+//         if(i === j) continue;
+
+//         if(bodyInfo[i][0] < bodyInfo[j][0] && bodyInfo[i][1] < bodyInfo[j][1]){
+//           score++;
+//         }
+//       }
+
+//       scoreList.push(score + 1);
+//     }
+
+//     console.log(scoreList.join(' '));
+
+//     process.exit();
+//   })
