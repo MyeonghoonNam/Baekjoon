3d0deb727478806b547b10dd200bcff9e8f8eec6
MyeongHoonNam <codenmh0822@naver.com>
2023-02-06 15:45:01 +0900

    문제 해결

--- 브루트 포스/1436.js
+++ 브루트 포스/1436.js
@@ -1,24 +1,51 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-rl.on('line', line => {
-  let N = parseInt(line);
-  let theNumberAtTheEnd = 665;
-  while(N > 0){
-    theNumberAtTheEnd++;
+const solution = () => {
+  let N = Number(input());
+  let currentNumber = 665;
 
-    if(theNumberAtTheEnd.toString().includes('666')){
-      N--;
+  while (N > 0) {
+    currentNumber += 1;
+
+    if (String(currentNumber).includes("666")) {
+      N -= 1;
     }
   }
 
-  console.log(theNumberAtTheEnd);
-  rl.close();
-})
-  .on('close', () => {
-    process.exit();
-  })
+  return currentNumber;
+};
+
+console.log(solution());
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input:process.stdin,
+//   output:process.stdout
+// });
+
+// rl.on('line', line => {
+//   let N = parseInt(line);
+//   let theNumberAtTheEnd = 665;
+//   while(N > 0){
+//     theNumberAtTheEnd++;
+
+//     if(theNumberAtTheEnd.toString().includes('666')){
+//       N--;
+//     }
+//   }
+
+//   console.log(theNumberAtTheEnd);
+//   rl.close();
+// })
+//   .on('close', () => {
+//     process.exit();
+//   })
