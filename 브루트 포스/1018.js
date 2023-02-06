c0738ae49da25254b3ef2800a4fe041dcef49834
MyeongHoonNam <codenmh0822@naver.com>
2023-02-06 15:32:14 +0900

    문제 해결

--- 브루트 포스/1018.js
+++ 브루트 포스/1018.js
@@ -1,80 +1,176 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `9 23
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBW`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', line => {
-  input.push(line.split(' '));
-})
-  .on('close', () => {
-    const N = input[0][0];
-    const M = input[0][1];
-    const board = input.slice(1);
-    const WB = [
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW"
-    ];
-    
-    const BW = [
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB",
-      "BWBWBWBW",
-      "WBWBWBWB"
-    ]
+const solution = () => {
+  const [N, M] = input().split(" ").map(Number);
+  const board = [];
+  let result = Number.MAX_SAFE_INTEGER;
 
-    let min = 100;
-    for(let i = 0; i + 8 <= N; i++){
-      for(let j = 0; j + 8 <= M; j++){
-        let temp = 0 ;
-        temp = Math.min(WB_Count(i, j), BW_Count(i, j));
-        if(temp < min){
-          min = temp;
+  for (let i = 0; i < N; i++) {
+    board.push(input());
+  }
+
+  const WhiteBoard = [
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+  ];
+
+  const BlackBoard = [
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+  ];
+
+  const getRepaintWhiteBoardCount = (x, y) => {
+    let count = 0;
+
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (board[i + y][j + x] !== WhiteBoard[i][j]) {
+          count++;
         }
       }
     }
 
-    console.log(min);
+    return count;
+  };
 
-    function WB_Count(x, y){
-      let repaintCnt = 0;
-      for(let i = 0; i < 8; i++){
-        for(let j = 0; j < 8; j++){
-          if(board[x+i][0][y+j] !== WB[i][j]){
-            repaintCnt++;
-          }
+  const getRepaintBlackBoardCount = (x, y) => {
+    let count = 0;
+
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (board[i + y][j + x] !== BlackBoard[i][j]) {
+          count++;
         }
       }
-
-      return repaintCnt;
     }
 
-    function BW_Count(x, y){
-      let repaintCnt = 0;
-      for(let i = 0; i < 8; i++){
-        for(let j = 0; j < 8; j++){
-          if(board[x+i][0][y+j] !== BW[i][j]){
-            repaintCnt++;
-          }
-        }
+    return count;
+  };
+
+  for (let i = 0; i + 8 <= N; i++) {
+    for (let j = 0; j + 8 <= M; j++) {
+      const minBoardCount = Math.min(
+        getRepaintWhiteBoardCount(j, i),
+        getRepaintBlackBoardCount(j, i)
+      );
+
+      if (minBoardCount < result) {
+        result = minBoardCount;
       }
-
-      return repaintCnt;
     }
+  }
 
-    process.exit();
-  });
+  return result;
+};
 
+console.log(solution());
 
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input:process.stdin,
+//   output:process.stdout
+// });
+
+// const input = [];
+// rl.on('line', line => {
+//   input.push(line.split(' '));
+// })
+//   .on('close', () => {
+//     const N = input[0][0];
+//     const M = input[0][1];
+//     const board = input.slice(1);
+//     const WB = [
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW"
+//     ];
+
+//     const BW = [
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB",
+//       "BWBWBWBW",
+//       "WBWBWBWB"
+//     ]
+
+//     let min = 100;
+//     for(let i = 0; i + 8 <= N; i++){
+//       for(let j = 0; j + 8 <= M; j++){
+//         let temp = 0 ;
+//         temp = Math.min(WB_Count(i, j), BW_Count(i, j));
+//         if(temp < min){
+//           min = temp;
+//         }
+//       }
+//     }
+
+//     console.log(min);
+
+//     function WB_Count(x, y){
+//       let repaintCnt = 0;
+//       for(let i = 0; i < 8; i++){
+//         for(let j = 0; j < 8; j++){
+//           if(board[x+i][0][y+j] !== WB[i][j]){
+//             repaintCnt++;
+//           }
+//         }
+//       }
+
+//       return repaintCnt;
+//     }
+
+//     function BW_Count(x, y){
+//       let repaintCnt = 0;
+//       for(let i = 0; i < 8; i++){
+//         for(let j = 0; j < 8; j++){
+//           if(board[x+i][0][y+j] !== BW[i][j]){
+//             repaintCnt++;
+//           }
+//         }
+//       }
+
+//       return repaintCnt;
+//     }
+
+//     process.exit();
+//   });
