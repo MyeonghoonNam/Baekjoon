f3216449af1f3d9dc2f50f7605fc78c7d65dc72f
MyeongHoonNam <codenmh0822@naver.com>
2023-03-24 14:22:58 +0900

    문제 해결

--- 정렬/2751.js
+++ 정렬/2751.js
@@ -1,28 +1,31 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+5
+4
+3
+2
+1`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-let input = [];
-rl.on('line', line => {
-  input.push(parseInt(line));
-})
-  .on('close', () => {
-    const T = input[0];
-    const numbers = input.slice(1);
+const solution = () => {
+  const N = Number(input());
+  const numbers = [];
 
-    let result = '';
+  for (let i = 0; i < N; i++) {
+    numbers.push(Number(input()));
+  }
 
-    numbers.sort((a, b) => {
-      return a - b;
-    });
+  const result = numbers.sort((a, b) => a - b).join("\n");
 
-    numbers.forEach(num => {
-      result += `${num}\n`;
-    })
+  return result;
+};
 
-    console.log(result);
-    process.exit();
-  })
+console.log(solution());
