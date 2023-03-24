74e7e9de224b461bb87e815fbdf2029def206ae7
MyeongHoonNam <codenmh0822@naver.com>
2023-03-24 14:48:16 +0900

    문제 해결

--- 정렬/11651.js
+++ 정렬/11651.js
@@ -1,27 +1,38 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+0 4
+1 2
+1 -1
+2 2
+3 3`
+).split("\n");
 
-const rl = readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', line => {
-  input.push(line.split(' ').map(el => parseInt(el)));
-})
-  .on('close', () => {
-    const T = input[0];
-    const locationArray = input.slice(1);
-    
-    locationArray.sort((a, b) => {
-      return a[1] - b[1] || a[0] - b[0];
-    })
+const solution = () => {
+  const N = Number(input());
+  const coordinate = [];
 
-    let result = '';
-    for(let i = 0; i < T; i++){
-      result += `${locationArray[i][0]} ${locationArray[i][1]}\n`
-    }
+  for (let i = 0; i < N; i++) {
+    const [x, y] = input().split(" ").map(Number);
+    coordinate.push({ x, y });
+  }
 
-    console.log(result);
-    process.exit();
-  })
+  coordinate.sort((a, b) => a.y - b.y || a.x - b.x);
+
+  let result = "";
+
+  for (let { x, y } of coordinate) {
+    result += `${x} ${y}\n`;
+  }
+
+  return result;
+};
+
+console.log(solution());
