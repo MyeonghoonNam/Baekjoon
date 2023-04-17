1003de86158f2b607e42c1355b6cffebd2338054
MyeongHoonNam <codenmh0822@naver.com>
2023-04-17 17:20:23 +0900

    문제 해결 - 중복 조합

--- 백트래킹/15652.js
+++ 백트래킹/15652.js
@@ -1,32 +1,36 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `4 2`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  const [N, M] = input[0].split(' ').map(Number);
-  const arr = [];
-  let result = '';
+const solution = () => {
+  const [N, M] = input().split(" ").map(Number);
+  const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+  const selected = [];
+  const result = [];
 
-  Dfs(1, 0);
-  console.log(result);
-
-  function Dfs(idx, cnt) {
+  const dfs = (idx, cnt) => {
     if (cnt === M) {
-      result += arr.join(' ') + '\n';
+      result.push(selected.join(" "));
       return;
     }
 
-    for (let i = idx; i <= N; i++) {
-      arr[cnt] = i;
-      Dfs(i, cnt + 1);
+    for (let i = idx; i < N; i++) {
+      selected[cnt] = numbers[i];
+      dfs(i, cnt + 1);
     }
-  }
-});
+  };
+
+  dfs(0, 0);
+
+  return result.join("\n");
+};
+
+console.log(solution());
