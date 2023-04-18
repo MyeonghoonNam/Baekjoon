4750affba42e76c25e072d9a58816f46785d2fdb
MyeongHoonNam <codenmh0822@naver.com>
2023-04-18 23:05:15 +0900

    문제 해결 - 중복 조합

--- 백트래킹/15657.js
+++ 백트래킹/15657.js
@@ -1,38 +1,41 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `4 2
+9 8 7 1`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  const [N, M] = input.shift().split(' ').map(Number);
-  const numbers = input
-    .shift()
-    .split(' ')
+const solution = () => {
+  const [N, M] = input().split(" ").map(Number);
+  const numbers = input()
+    .split(" ")
     .map(Number)
     .sort((a, b) => a - b);
 
-  const arr = [];
-  let result = '';
+  const selected = [];
+  const result = [];
 
-  Dfs(0, 0);
-  console.log(result);
-
-  function Dfs(idx, cnt) {
+  const dfs = (idx, cnt) => {
     if (cnt === M) {
-      result += arr.join(' ') + '\n';
+      result.push(selected.join(" "));
       return;
     }
 
     for (let i = idx; i < N; i++) {
-      arr[cnt] = numbers[i];
-      Dfs(i, cnt + 1);
+      selected[cnt] = numbers[i];
+      dfs(i, cnt + 1);
     }
-  }
-});
+  };
+
+  dfs(0, 0);
+
+  return result.join("\n");
+};
+
+console.log(solution());
