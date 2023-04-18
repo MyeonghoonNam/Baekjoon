523ed9c336bd2a006bb56a0a50317ec2d61c8637
MyeongHoonNam <codenmh0822@naver.com>
2023-04-18 22:54:45 +0900

    문제 해결 - 조합

--- 백트래킹/15655.js
+++ 백트래킹/15655.js
@@ -1,43 +1,53 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `4 4
+1231 1232 1233 1234`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  const [N, M] = input.shift().split(' ').map(Number);
-  const numbers = input
-    .shift()
-    .split(' ')
+const solution = () => {
+  const [N, M] = input().split(" ").map(Number);
+  const numbers = input()
+    .split(" ")
     .map(Number)
     .sort((a, b) => a - b);
 
   const visited = new Array(N).fill(false);
-  const arr = [];
-  let result = '';
+  const reuslt = [];
 
-  Dfs(0, 0);
-  console.log(result);
-
-  function Dfs(idx, cnt) {
+  const dfs = (idx, cnt) => {
     if (cnt === M) {
-      result += arr.join(' ') + '\n';
+      const selected = [];
+
+      for (let i = 0; i < N; i++) {
+        if (visited[i]) {
+          selected.push(numbers[i]);
+        }
+      }
+
+      reuslt.push(selected.join(" "));
+
       return;
     }
 
     for (let i = idx; i < N; i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        arr[cnt] = numbers[i];
-        Dfs(i + 1, cnt + 1);
-        visited[i] = false;
-      }
+      if (visited[i]) continue;
+
+      visited[i] = true;
+      dfs(i, cnt + 1);
+      visited[i] = false;
     }
-  }
-});
+  };
+
+  dfs(0, 0);
+
+  return reuslt.join("\n");
+};
+
+console.log(solution());
