227054d3f27dcbed12ab2bdbcd57d5b7ba261161
MyeongHoonNam <codenmh0822@naver.com>
2023-04-20 15:11:53 +0900

    문제 해결

--- 브루트 포스/6603.js
+++ 브루트 포스/6603.js
@@ -1,53 +1,58 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `7 1 2 3 4 5 6 7
 8 1 2 3 5 8 13 21 34
 0`
-).split('\n');
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-Solution();
-
-function Solution() {
-  let K = 0;
-  let S = [];
-
-  const selected = new Array(K).fill(false);
+const solution = () => {
   const result = [];
 
-  const dfs = (idx, cnt) => {
-    if (cnt === 6) {
-      console.log(result.join(' '));
-      return;
-    }
+  while (true) {
+    const [K, ...S] = input().split(" ").map(Number);
 
-    for (let i = idx; i < K; i++) {
-      if (!selected[i]) {
-        selected[i] = true;
-        result[cnt] = S[i];
-        dfs(i, cnt + 1);
-        selected[i] = false;
+    if (K === 0) break;
+
+    const MAX_SELECTED_COUNT = 6;
+    const visited = new Array(K).fill(false);
+
+    const dfs = (idx, cnt) => {
+      if (cnt === MAX_SELECTED_COUNT) {
+        const selected = [];
+
+        for (let i = 0; i < K; i++) {
+          if (visited[i] === true) {
+            selected.push(S[i]);
+          }
+        }
+
+        result.push(selected.join(" "));
+        return;
       }
-    }
-  };
 
-  while (1) {
-    const testcase = input().split(' ').map(Number);
-
-    if (testcase[0] === 0) break;
-
-    K = testcase[0];
-    S = testcase.slice(1);
+      for (let i = idx; i < K; i++) {
+        if (visited[i] === false) {
+          visited[i] = true;
+          dfs(i, cnt + 1);
+          visited[i] = false;
+        }
+      }
+    };
 
     dfs(0, 0);
-    console.log();
+    result.push("");
   }
-}
+
+  result.pop();
+
+  return result.join("\n");
+};
+
+console.log(solution());
