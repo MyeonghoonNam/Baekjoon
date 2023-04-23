6d17671ccea70fc99979f424d7334c6c45fdd348
MyeongHoonNam <codenmh0822@naver.com>
2023-04-23 22:29:01 +0900

    문제 해결

--- 그래프/1012.js
+++ 그래프/1012.js
@@ -2,14 +2,27 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `1
-5 3 6
-0 2
-1 2
-2 2
-3 2
+    : `2
+10 8 17
+0 0
+1 0
+1 1
 4 2
-4 0`
+4 3
+4 5
+2 4
+3 4
+7 4
+8 4
+9 4
+7 5
+8 5
+9 5
+7 6
+8 6
+9 6
+10 10 1
+5 5`
 ).split("\n");
 
 const input = (() => {
@@ -17,23 +30,22 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
-  let T = Number(input());
   const result = [];
-
+  let T = Number(input());
   while (T--) {
     const [M, N, K] = input().split(" ").map(Number);
-    const map = Array.from(new Array(N), () => new Array(M).fill(0));
+    const graph = Array.from(new Array(N), () => new Array(M).fill(0));
     const visited = Array.from(new Array(N), () => new Array(M).fill(false));
+    const dx = [0, 0, -1, 1];
+    const dy = [-1, 1, 0, 0];
     let count = 0;
 
     for (let i = 0; i < K; i++) {
-      const [x, y] = input().split(" ").map(Number);
-      map[y][x] = 1;
+      const [X, Y] = input().split(" ").map(Number);
+      graph[Y][X] = 1;
     }
-
-    const dx = [0, 0, -1, 1];
-    const dy = [-1, 1, 0, 0];
 
     const dfs = (y, x) => {
       visited[y][x] = true;
@@ -44,20 +56,20 @@ const solution = () => {
 
         if (!checkMapRange(ny, nx)) continue;
 
-        if (map[ny][nx] === 1 && !visited[ny][nx]) {
+        if (!visited[ny][nx] && graph[ny][nx] === 1) {
           dfs(ny, nx);
         }
       }
     };
 
     const checkMapRange = (y, x) => {
-      if (x >= 0 && x < M && y >= 0 && y < N) return true;
+      if (x >= 0 && y >= 0 && x < M && y < N) return true;
       else return false;
     };
 
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (!visited[i][j] && map[i][j] === 1) {
+        if (!visited[i][j] && graph[i][j] === 1) {
           dfs(i, j);
           count += 1;
         }
@@ -69,5 +81,59 @@ const solution = () => {
 
   return result.join("\n");
 };
+
+// 1차 해결
+// const solution = () => {
+//   let T = Number(input());
+//   const result = [];
+
+//   while (T--) {
+//     const [M, N, K] = input().split(" ").map(Number);
+//     const map = Array.from(new Array(N), () => new Array(M).fill(0));
+//     const visited = Array.from(new Array(N), () => new Array(M).fill(false));
+//     let count = 0;
+
+//     for (let i = 0; i < K; i++) {
+//       const [x, y] = input().split(" ").map(Number);
+//       map[y][x] = 1;
+//     }
+
+//     const dx = [0, 0, -1, 1];
+//     const dy = [-1, 1, 0, 0];
+
+//     const dfs = (y, x) => {
+//       visited[y][x] = true;
+
+//       for (let i = 0; i < 4; i++) {
+//         const nx = x + dx[i];
+//         const ny = y + dy[i];
+
+//         if (!checkMapRange(ny, nx)) continue;
+
+//         if (map[ny][nx] === 1 && !visited[ny][nx]) {
+//           dfs(ny, nx);
+//         }
+//       }
+//     };
+
+//     const checkMapRange = (y, x) => {
+//       if (x >= 0 && x < M && y >= 0 && y < N) return true;
+//       else return false;
+//     };
+
+//     for (let i = 0; i < N; i++) {
+//       for (let j = 0; j < M; j++) {
+//         if (!visited[i][j] && map[i][j] === 1) {
+//           dfs(i, j);
+//           count += 1;
+//         }
+//       }
+//     }
+
+//     result.push(count);
+//   }
+
+//   return result.join("\n");
+// };
 
 console.log(solution());
