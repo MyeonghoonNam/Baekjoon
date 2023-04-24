5d05be1cf41a261948313b9bc9c3fb19bf8855d3
MyeongHoonNam <codenmh0822@naver.com>
2023-04-24 22:17:19 +0900

    문제 해결

--- 그래프/10026.js
+++ 그래프/10026.js
@@ -1,103 +1,86 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `5
 RRRBB
 GGBBB
 BBBRR
 BBRRR
 RRRRR`
-).split('\n');
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-console.log(Solution());
-
-function Solution() {
+const solution = () => {
   const N = Number(input());
   const map = [];
-  let visited = Array.from(new Array(N), () => new Array(N).fill(false));
-  const result = [];
+  let answer1 = 0;
+  let answer2 = 0;
+
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
 
   for (let i = 0; i < N; i++) {
-    map[i] = input().split('');
+    map.push(input().split(""));
   }
 
-  const init = () => {
-    visited = Array.from(new Array(N), () => new Array(N).fill(false));
+  let visited = Array.from(new Array(N), () => new Array(N).fill(false));
 
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < N; j++) {
-        if (map[i][j] === 'R') {
-          map[i][j] = 'G';
-        }
+  const dfs = (x, y, color) => {
+    visited[x][y] = true;
+
+    for (let i = 0; i < 4; i++) {
+      const nx = dx[i] + x;
+      const ny = dy[i] + y;
+
+      if (!checkMapRange(nx, ny)) continue;
+
+      if (!visited[nx][ny] && map[nx][ny] === color) {
+        dfs(nx, ny, color);
       }
     }
   };
 
-  const checkRange = (x, y) => {
-    if (x >= 0 && x < N && y >= 0 && y < N) return true;
+  const checkMapRange = (x, y) => {
+    if (x >= 0 && y >= 0 && x < N && y < N) return true;
     else return false;
   };
 
-  const bfs = (pos) => {
-    const q = [pos];
-    let qIdx = 0;
-
-    visited[pos[0]][pos[1]] = true;
-
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
-
-    while (qIdx < q.length) {
-      const [x, y] = q[qIdx++];
-      const color = map[x][y];
-
-      for (let i = 0; i < 4; i++) {
-        const nx = x + dx[i];
-        const ny = y + dy[i];
-
-        if (!checkRange(nx, ny)) continue;
-
-        if (map[nx][ny] === color && !visited[nx][ny]) {
-          visited[nx][ny] = true;
-          q.push([nx, ny]);
-        }
-      }
-    }
-  };
-
-  let answer1 = 0;
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
       if (!visited[i][j]) {
-        bfs([i, j]);
-        answer1++;
+        dfs(i, j, map[i][j]);
+        answer1 += 1;
       }
     }
   }
 
-  result.push(answer1);
-
-  init();
-
-  let answer2 = 0;
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (!visited[i][j]) {
-        bfs([i, j]);
-        answer2++;
+      if (map[i][j] === "G") {
+        map[i][j] = "R";
       }
     }
   }
 
-  result.push(answer2);
+  visited = Array.from(new Array(N), () => new Array(N).fill(false));
 
-  return result.join(' ');
-}
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (!visited[i][j]) {
+        dfs(i, j, map[i][j]);
+        answer2 += 1;
+      }
+    }
+  }
+
+  const result = `${answer1} ${answer2}`;
+
+  return result;
+};
+
+console.log(solution());
