cd787189d5cd43452f2dc27e920559c506ef4c25
MyeongHoonNam <codenmh0822@naver.com>
2023-04-24 10:42:09 +0900

    문제 2차 해결

--- 그래프/1240.js
+++ 그래프/1240.js
@@ -15,10 +15,12 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결 distance 거리 배열 활용
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
   const graph = Array.from(new Array(N + 1), () => []);
   let visited = new Array(N + 1).fill(false);
+  let distance = new Array(N + 1).fill(Infinity);
   const result = [];
 
   for (let i = 0; i < N - 1; i++) {
@@ -28,19 +30,15 @@ const solution = () => {
     graph[end].push({ node: start, dist });
   }
 
-  const dfs = (current, end, dist) => {
-    visited[current] = true;
+  const dfs = (node, dist) => {
+    visited[node] = true;
+    distance[node] = dist;
 
-    if (current === end) {
-      result.push(dist);
-      return;
-    }
-
-    for (let i = 0; i < graph[current].length; i++) {
-      const next = graph[current][i];
+    for (let i = 0; i < graph[node].length; i++) {
+      const next = graph[node][i];
 
       if (!visited[next.node]) {
-        dfs(next.node, end, dist + next.dist);
+        dfs(next.node, dist + next.dist);
       }
     }
   };
@@ -48,11 +46,54 @@ const solution = () => {
   for (let i = 0; i < M; i++) {
     const [start, end] = input().split(" ").map(Number);
 
-    dfs(start, end, 0);
+    dfs(start, 0);
+    result.push(distance[end]);
     visited = new Array(N + 1).fill(false);
+    distance = new Array(N + 1).fill(Infinity);
   }
 
   return result.join("\n");
 };
+
+// 1차 해결
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const graph = Array.from(new Array(N + 1), () => []);
+//   let visited = new Array(N + 1).fill(false);
+//   const result = [];
+
+//   for (let i = 0; i < N - 1; i++) {
+//     const [start, end, dist] = input().split(" ").map(Number);
+
+//     graph[start].push({ node: end, dist });
+//     graph[end].push({ node: start, dist });
+//   }
+
+//   const dfs = (current, end, dist) => {
+//     visited[current] = true;
+
+//     if (current === end) {
+//       result.push(dist);
+//       return;
+//     }
+
+//     for (let i = 0; i < graph[current].length; i++) {
+//       const next = graph[current][i];
+
+//       if (!visited[next.node]) {
+//         dfs(next.node, end, dist + next.dist);
+//       }
+//     }
+//   };
+
+//   for (let i = 0; i < M; i++) {
+//     const [start, end] = input().split(" ").map(Number);
+
+//     dfs(start, end, 0);
+//     visited = new Array(N + 1).fill(false);
+//   }
+
+//   return result.join("\n");
+// };
 
 console.log(solution());
