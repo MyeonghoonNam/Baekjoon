9896c53299ca7221bfbc24d5358e37fb04942f34
MyeongHoonNam <codenmh0822@naver.com>
2023-04-25 19:22:39 +0900

    문제 해결

--- 그래프/7562.js
+++ 그래프/7562.js
@@ -1,78 +1,119 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `3
+8
+0 0
+7 0
+100
+0 0
+30 50
+10
+1 1
+1 1`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  const T = parseInt(input.shift());
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-  let L = 0;
-  let start = [];
-  let end = [];
-  let visited = [];
-
-  for (let i = 0; i < T; i++) {
-    L = parseInt(input.shift());
-    visited = Array.from(new Array(L), () => new Array(L).fill(false));
-
-    start = input
-      .shift()
-      .split(' ')
-      .map((el) => parseInt(el));
-    end = input
-      .shift()
-      .split(' ')
-      .map((el) => parseInt(el));
-
-    Bfs([...start, 0]); // 0은 카운터 표시를 위함
+class Queue {
+  constructor() {
+    this.head = this.tail = null;
+    this.size = 0;
   }
 
-  process.exit();
+  enqueue(value) {
+    const newNode = new Node(value);
 
-  function Bfs(location) {
-    const q = [];
-    q.push(location);
+    if (this.isEmpty()) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
 
-    let count = 0;
-    while (q.length !== 0) {
-      const current = q.shift();
+    this.size += 1;
+  }
 
-      const x = current[0];
-      const y = current[1];
-      count = current[2];
+  dequeue() {
+    if (this.isEmpty()) return;
 
-      if (x === end[0] && y === end[1]) {
-        console.log(count);
-        return;
-      }
+    const popNode = this.head;
+    this.head = popNode.next;
 
-      const dx = [-1, -2, -2, -1, 1, 2, 2, 1];
-      const dy = [-2, -1, 1, 2, 2, 1, -1, -2];
+    if (this.size === 1) {
+      this.tail = this.head;
+    }
 
-      for (let i = 0; i < 9; i++) {
-        const next_X = x + dx[i];
-        const next_Y = y + dy[i];
+    this.size -= 1;
 
-        if (CheckRange(next_X, next_Y) && !visited[next_X][next_Y]) {
-          visited[next_X][next_Y] = true;
-          q.push([next_X, next_Y, count + 1]);
+    return popNode.value;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
+}
+
+const solution = () => {
+  const result = [];
+  let T = Number(input());
+
+  while (T--) {
+    const L = Number(input());
+    const [cx, cy] = input().split(" ").map(Number);
+    const [tx, ty] = input().split(" ").map(Number);
+    const visited = Array.from(new Array(L), () => new Array(L).fill(false));
+    const dx = [-2, -2, -1, 1, 2, 2, 1, -1];
+    const dy = [-1, 1, 2, 2, 1, -1, -2, -2];
+
+    const bfs = () => {
+      const queue = new Queue();
+
+      queue.enqueue([cx, cy, 0]);
+      visited[cx][cy] = true;
+
+      while (!queue.isEmpty()) {
+        const [x, y, cost] = queue.dequeue();
+
+        if (x === tx && y === ty) {
+          result.push(cost);
+          return;
+        }
+
+        for (let i = 0; i < 8; i++) {
+          const nx = x + dx[i];
+          const ny = y + dy[i];
+
+          if (!checkMapRange(nx, ny)) continue;
+
+          if (!visited[nx][ny]) {
+            queue.enqueue([nx, ny, cost + 1]);
+            visited[nx][ny] = true;
+          }
         }
       }
-    }
+    };
+
+    const checkMapRange = (x, y) => {
+      if (x >= 0 && y >= 0 && x < L && y < L) return true;
+      else return false;
+    };
+
+    bfs();
   }
 
-  function CheckRange(x, y) {
-    if (x >= 0 && x < L && y >= 0 && y < L) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-});
+  return result.join("\n");
+};
+
+console.log(solution());
