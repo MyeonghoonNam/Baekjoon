d749e0f663dee3d1f6294caf1723bcf8217988f3
MyeongHoonNam <codenmh0822@naver.com>
2023-04-26 14:35:23 +0900

    문제 해결

--- 그래프/14395.js
+++ 그래프/14395.js
@@ -1,69 +1,111 @@
-'use strict';
-
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `10 1`
-).split('\n');
+).split("\n");
 
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-console.log(Solution());
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-function Solution() {
-  const [S, T] = input().split(' ').map(Number);
+class Queue {
+  constructor() {
+    this.head = this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.size += 1;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return;
+
+    const popNode = this.head;
+    this.head = popNode.next;
+
+    if (this.size === 1) {
+      this.tail = this.head;
+    }
+
+    this.size -= 1;
+
+    return popNode.value;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
+}
+
+const solution = () => {
+  const [S, T] = input().split(" ").map(Number);
+  const operator = ["*", "+", "-", "/"];
   const visited = new Set();
+  let isFound = false;
+  let result = "";
 
-  let result = 0;
-
-  if (S === T) return result;
-  else result = -1;
+  if (S === T) return 0;
 
   const bfs = () => {
-    const q = [[S, '']];
+    const queue = new Queue();
 
+    queue.enqueue([S, ""]);
     visited.add(S);
 
-    while (q.length > 0) {
-      const [curNum, useMod] = q.shift();
+    while (!queue.isEmpty()) {
+      let [current, oper] = queue.dequeue();
 
-      if (curNum === T) {
-        result = useMod;
+      if (current === T) {
+        result = oper;
+        isFound = true;
+
         return;
       }
 
-      for (let i = 0; i < 4; i++) {
-        let nextNum = curNum;
-        let updateMod = useMod;
+      for (let i = 0; i < operator.length; i++) {
+        const mod = operator[i];
+        let number = current;
 
-        switch (i) {
-          case 0:
-            nextNum *= curNum;
-            updateMod += '*';
+        switch (mod) {
+          case "*":
+            number *= number;
             break;
-          case 1:
-            nextNum += curNum;
-            updateMod += '+';
+          case "+":
+            number += number;
             break;
-          case 2:
-            nextNum -= curNum;
-            updateMod += '-';
+          case "-":
+            number -= number;
             break;
-          case 3:
-            if (curNum !== 0) {
-              nextNum = parseInt(nextNum / nextNum);
-              updateMod += '/';
-              break;
+          case "/":
+            if (current !== 0) {
+              number /= number;
             }
+
+            break;
         }
 
-        if (!visited.has(nextNum)) {
-          visited.add(nextNum);
-          q.push([nextNum, updateMod]);
+        if (!visited.has(number)) {
+          visited.add(number);
+          queue.enqueue([number, oper + mod]);
         }
       }
     }
@@ -71,5 +113,9 @@ function Solution() {
 
   bfs();
 
+  if (!isFound) return -1;
+
   return result;
-}
+};
+
+console.log(solution());
