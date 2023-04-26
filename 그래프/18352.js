dcb8b649c1ad4c924ff792b12a20fbf2c7725681
MyeongHoonNam <codenmh0822@naver.com>
2023-04-26 19:21:42 +0900

    문제 3차 해결 - 다익스트라

--- 그래프/18352.js
+++ 그래프/18352.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `4 4 1 1
+    : `4 4 2 1
 1 2
 1 3
 2 3
@@ -29,99 +29,222 @@ const input = (() => {
  *  3-1. 도달가능한 다음 노드의 최단거리 = 현재 노드의 최단거리 + 1
  */
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
+class MinHeap {
   constructor() {
-    this.head = this.tail = null;
+    this.heap = [];
     this.size = 0;
   }
 
-  enqeueu(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-
+  insert(value) {
+    this.heap.push(value);
     this.size += 1;
+    this.moveUp();
   }
 
-  dequeue() {
-    if (this.isEmpty()) return;
-
-    const popNode = this.head;
-    this.head = popNode.next;
-
+  pop() {
     if (this.size === 1) {
-      this.tail = this.head;
+      this.size -= 1;
+      return this.heap.pop();
     }
 
-    this.size -= 1;
+    const minValue = this.heap[0];
 
-    return popNode.value;
+    this.heap[0] = this.heap.pop();
+    this.size -= 1;
+    this.moveDown();
+
+    return minValue;
+  }
+
+  moveUp() {
+    let currentIndex = this.size - 1;
+
+    while (currentIndex > 0) {
+      const parentIndex = parseInt((currentIndex - 1) / 2);
+
+      if (this.heap[parentIndex].cost <= this.heap[currentIndex].cost) break;
+
+      this.swap(parentIndex, currentIndex);
+      currentIndex = parentIndex;
+    }
+  }
+
+  moveDown(currentIndex = 0) {
+    const leftChildIndex = currentIndex * 2 + 1;
+    const rightChildIndex = currentIndex * 2 + 2;
+    let minIndex = currentIndex;
+
+    while (
+      leftChildIndex < this.size &&
+      this.heap[leftChildIndex].cost < this.heap[minIndex].cost
+    ) {
+      minIndex = leftChildIndex;
+    }
+
+    while (
+      rightChildIndex < this.size &&
+      this.heap[rightChildIndex].cost < this.heap[minIndex].cost
+    ) {
+      minIndex = rightChildIndex;
+    }
+
+    if (minIndex !== currentIndex) {
+      this.swap(minIndex, currentIndex);
+      this.moveDown(minIndex);
+    }
   }
 
   isEmpty() {
     return this.size === 0 ? true : false;
   }
+
+  swap(a, b) {
+    let temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
+  }
 }
 
-// 2차 해결
+// 3차 해결
 const solution = () => {
   const [N, M, K, X] = input().split(" ").map(Number);
   const graph = Array.from(new Array(N + 1), () => []);
-  const visited = new Array(N + 1).fill(false);
   const distance = new Array(N + 1).fill(Infinity);
-  const result = [];
-  let isFound = false;
 
   for (let i = 0; i < M; i++) {
     const [start, end] = input().split(" ").map(Number);
-    graph[start].push(end);
+    graph[start].push({ node: end, cost: 1 });
   }
 
-  const bfs = () => {
-    const queue = new Queue();
+  const dijkstra = (X) => {
+    const minHeap = new MinHeap();
 
-    queue.enqeueu(X);
-    visited[X] = true;
+    minHeap.insert({ node: X, cost: 0 });
     distance[X] = 0;
 
-    while (!queue.isEmpty()) {
-      const node = queue.dequeue();
+    while (!minHeap.isEmpty()) {
+      const { node, cost } = minHeap.pop();
 
-      if (distance[node] === K) {
-        result.push(node);
-        isFound = true;
-      }
+      if (distance[node] < cost) continue;
 
       for (let i = 0; i < graph[node].length; i++) {
         const next = graph[node][i];
+        const totalCost = cost + next.cost;
 
-        if (!visited[next]) {
-          queue.enqeueu(next);
-          visited[next] = true;
-          distance[next] = distance[node] + 1;
+        if (totalCost < distance[next.node]) {
+          minHeap.insert({ node: next.node, cost: totalCost });
+          distance[next.node] = totalCost;
         }
       }
     }
   };
 
-  bfs();
+  dijkstra(X);
 
-  if (!isFound) return -1;
+  let result = [];
 
-  return result.sort((a, b) => a - b).join("\n");
+  for (let i = 1; i <= N; i++) {
+    if (distance[i] === K) {
+      result.push(i);
+    }
+  }
+
+  return result.length === 0 ? -1 : result.join("\n");
 };
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// class Queue {
+//   constructor() {
+//     this.head = this.tail = null;
+//     this.size = 0;
+//   }
+
+//   enqeueu(value) {
+//     const newNode = new Node(value);
+
+//     if (this.isEmpty()) {
+//       this.head = this.tail = newNode;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+
+//     this.size += 1;
+//   }
+
+//   dequeue() {
+//     if (this.isEmpty()) return;
+
+//     const popNode = this.head;
+//     this.head = popNode.next;
+
+//     if (this.size === 1) {
+//       this.tail = this.head;
+//     }
+
+//     this.size -= 1;
+
+//     return popNode.value;
+//   }
+
+//   isEmpty() {
+//     return this.size === 0 ? true : false;
+//   }
+// }
+
+// 2차 해결
+// const solution = () => {
+//   const [N, M, K, X] = input().split(" ").map(Number);
+//   const graph = Array.from(new Array(N + 1), () => []);
+//   const visited = new Array(N + 1).fill(false);
+//   const distance = new Array(N + 1).fill(Infinity);
+//   const result = [];
+//   let isFound = false;
+
+//   for (let i = 0; i < M; i++) {
+//     const [start, end] = input().split(" ").map(Number);
+//     graph[start].push(end);
+//   }
+
+//   const bfs = () => {
+//     const queue = new Queue();
+
+//     queue.enqeueu(X);
+//     visited[X] = true;
+//     distance[X] = 0;
+
+//     while (!queue.isEmpty()) {
+//       const node = queue.dequeue();
+
+//       if (distance[node] === K) {
+//         result.push(node);
+//         isFound = true;
+//       }
+
+//       for (let i = 0; i < graph[node].length; i++) {
+//         const next = graph[node][i];
+
+//         if (!visited[next]) {
+//           queue.enqeueu(next);
+//           visited[next] = true;
+//           distance[next] = distance[node] + 1;
+//         }
+//       }
+//     }
+//   };
+
+//   bfs();
+
+//   if (!isFound) return -1;
+
+//   return result.sort((a, b) => a - b).join("\n");
+// };
 
 // 1차 해결
 // const solution = () => {
