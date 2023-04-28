c8f589ffdc6163db41e1c25cf2037d43a0db25b6
MyeongHoonNam <codenmh0822@naver.com>
2023-04-28 20:40:52 +0900

    문제 해결

--- 동적계획법/2156.js
+++ 동적계획법/2156.js
@@ -1,37 +1,46 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `6
+6
+10
+13
+9
+8
+1`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+const solution = () => {
+  const N = Number(input());
+  const drink = [0];
 
-  function Solution(input) {
-    const N = Number(input.shift());
-    const arr = [0];
-    for (let i = 0; i < N; i++) {
-      const num = Number(input.shift());
-      arr.push(num);
-    }
-
-    const dp = [0];
-    dp[1] = arr[1];
-    dp[2] = dp[1] + arr[2];
-    for (let i = 3; i <= N; i++) {
-      dp[i] = Math.max(
-        dp[i - 1],
-        dp[i - 2] + arr[i],
-        dp[i - 3] + arr[i - 1] + arr[i]
-      );
-    }
-
-    return dp[N];
+  for (let i = 0; i < N; i++) {
+    drink.push(Number(input()));
   }
-});
+
+  const DP = [];
+
+  DP[0] = 0;
+  DP[1] = drink[1];
+  DP[2] = drink[1] + drink[2];
+
+  for (let i = 3; i <= N; i++) {
+    DP[i] = Math.max(
+      DP[i - 1],
+      DP[i - 2] + drink[i],
+      DP[i - 3] + drink[i - 1] + drink[i]
+    );
+  }
+
+  const result = DP[N];
+
+  return result;
+};
+
+console.log(solution());
