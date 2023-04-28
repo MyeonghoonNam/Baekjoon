49803b0498df7ffc8d4d61f4adb525497c9cd4fa
MyeongHoonNam <codenmh0822@naver.com>
2023-04-28 19:49:05 +0900

    문제 해결

--- 동적계획법/1003.js
+++ 동적계획법/1003.js
@@ -12,30 +12,56 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const result = [];
   let T = Number(input());
 
   while (T--) {
     const N = Number(input());
-    const MAX_NUMBER = 41;
-    const DP = Array.from(new Array(MAX_NUMBER), () => new Array(2));
+    const DP = new Array(N + 1).fill(0);
 
-    DP[0][0] = 1;
-    DP[0][1] = 0;
-    DP[1][0] = 0;
-    DP[1][1] = 1;
+    DP[0] = 0;
+    DP[1] = 1;
 
     for (let i = 2; i <= N; i++) {
-      for (let j = 0; j < 2; j++) {
-        DP[i][j] = DP[i - 2][j] + DP[i - 1][j];
-      }
+      DP[i] = DP[i - 1] + DP[i - 2];
     }
 
-    result.push(`${DP[N][0]} ${DP[N][1]}`);
+    if (N === 0) {
+      result.push("1 0");
+    } else {
+      result.push(`${DP[N - 1]} ${DP[N]}`);
+    }
   }
 
   return result.join("\n");
 };
+
+// const solution = () => {
+//   const result = [];
+//   let T = Number(input());
+
+//   while (T--) {
+//     const N = Number(input());
+//     const MAX_NUMBER = 41;
+//     const DP = Array.from(new Array(MAX_NUMBER), () => new Array(2));
+
+//     DP[0][0] = 1;
+//     DP[0][1] = 0;
+//     DP[1][0] = 0;
+//     DP[1][1] = 1;
+
+//     for (let i = 2; i <= N; i++) {
+//       for (let j = 0; j < 2; j++) {
+//         DP[i][j] = DP[i - 2][j] + DP[i - 1][j];
+//       }
+//     }
+
+//     result.push(`${DP[N][0]} ${DP[N][1]}`);
+//   }
+
+//   return result.join("\n");
+// };
 
 console.log(solution());
