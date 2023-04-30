924a0c467ba88f0ca40579b1db43b00a747fbc94
MyeongHoonNam <codenmh0822@naver.com>
2023-04-30 20:42:17 +0900

    문제 해결

---
+++ 구간합/11441.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+10 20 30 40 50
+5
+1 3
+2 4
+3 5
+1 5
+4 4`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const solution = () => {
+  const N = Number(input());
+  const numbers = input().split(" ").map(Number);
+  const M = Number(input());
+
+  const prefixSum = [0];
+  let sum = 0;
+
+  for (let i = 0; i < N; i++) {
+    sum += numbers[i];
+    prefixSum.push(sum);
+  }
+
+  const result = [];
+
+  for (let i = 0; i < M; i++) {
+    const [start, end] = input().split(" ").map(Number);
+    result.push(prefixSum[end] - prefixSum[start - 1]);
+  }
+
+  return result.join("\n");
+};
+
+console.log(solution());
