271eb512cc113e0001411769a651b11d93025fda
MyeongHoonNam <codenmh0822@naver.com>
2023-05-06 23:38:31 +0900

    문제 3차 해결

--- 유니온파인드/1717.js
+++ 유니온파인드/1717.js
@@ -1,73 +1,3 @@
-// 백준 런타임 에러 발생
-// 알고리즘 해결에는 문제 없음
-
-// const fs = require("fs");
-// const stdin = (
-//   process.platform === "linux"
-//     ? fs.readFileSync("/dev/stdin").toString()
-//     : `7 8
-// 0 1 3
-// 1 1 7
-// 0 7 6
-// 1 7 1
-// 0 3 7
-// 0 4 2
-// 0 1 1
-// 1 1 1`
-// ).split("\n");
-
-// const input = (() => {
-//   let line = 0;
-//   return () => stdin[line++];
-// })();
-
-// const solution = () => {
-//   const findParent = (parent, v) => {
-//     if (parent[v] !== v) {
-//       parent[v] = findParent(parent, parent[v]);
-//     }
-
-//     return parent[v];
-//   };
-
-//   const unionParent = (parent, start, end) => {
-//     start = findParent(parent, start);
-//     end = findParent(parent, end);
-
-//     if (start < end) {
-//       parent[end] = start;
-//     } else {
-//       parent[start] = end;
-//     }
-//   };
-
-//   const [N, M] = input().split(" ").map(Number);
-//   const parent = new Array(N + 1);
-//   let result = [];
-
-//   for (let i = 1; i <= N; i++) {
-//     parent[i] = i;
-//   }
-
-//   for (let i = 0; i < M; i++) {
-//     const [mod, start, end] = input().split(" ").map(Number);
-
-//     if (mod === 0) {
-//       unionParent(parent, start, end);
-//     } else {
-//       if (findParent(parent, start) === findParent(parent, end)) {
-//         result.push("YES");
-//       } else {
-//         result.push("NO");
-//       }
-//     }
-//   }
-
-//   return result.join("\n");
-// };
-
-// console.log(solution());
-
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -77,54 +7,53 @@ const rl = readline.createInterface({
 
 let input = [];
 
-const fun = (input) => {
-  const [n, m] = input
-    .shift()
-    .split(" ")
-    .map((x) => +x);
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
 
-  const a = input.map((v) => v.split(" ").map((x) => +x));
-
-  const b = new Array(n + 1).fill(null).map((_, idx) => idx);
+const solution = (input) => {
+  const [N, M] = input[0].split(" ").map(Number);
+  const parentTable = new Array(N + 1);
   const result = [];
 
-  const findParent = (x) => {
-    if (x === b[x]) return x;
-    b[x] = findParent(b[x]);
-    return b[x];
+  for (let i = 0; i <= N; i++) {
+    parentTable[i] = i;
+  }
+
+  const findParent = (parent, value) => {
+    if (parent[value] !== value) {
+      parent[value] = findParent(parent, parent[value]);
+    }
+
+    return parent[value];
   };
 
-  const isUnion = (x, y) => {
-    x = findParent(x);
-    y = findParent(y);
-    return x === y ? "YES" : "NO";
-  };
+  const unionParent = (parent, start, end) => {
+    start = findParent(parent, start);
+    end = findParent(parent, end);
 
-  const setUnion = (x, y) => {
-    x = findParent(x);
-    y = findParent(y);
-    if (x < y) {
-      b[y] = x;
+    if (start < end) {
+      parent[end] = start;
     } else {
-      b[x] = y;
+      parent[start] = end;
     }
   };
 
-  for (let i = 0; i < m; i++) {
-    const [c, x, y] = a[i];
-    if (c === 0) {
-      setUnion(x, y);
-    } else if (c === 1) {
-      result.push(isUnion(x, y));
+  for (let i = 1; i <= M; i++) {
+    const [mod, start, end] = input[i].split(" ").map(Number);
+
+    if (mod === 0) {
+      unionParent(parentTable, start, end);
+    } else if (mod === 1) {
+      const isInclude =
+        findParent(parentTable, start) === findParent(parentTable, end);
+
+      result.push(isInclude ? "YES" : "NO");
     }
   }
 
   console.log(result.join("\n"));
 };
-
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  fun(input);
-  process.exit();
-});
