ae27382e2b162bcfc6df72a329bfd01f97a6235d
MyeongHoonNam <codenmh0822@naver.com>
2023-05-10 17:32:42 +0900

    문제 해결

--- 투 포인터/1806.js
+++ 투 포인터/1806.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `1 2
-2`
+    : `10 15
+5 1 3 5 10 7 4 9 2 8`
 ).split("\n");
 
 const input = (() => {
@@ -11,27 +11,52 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const [N, S] = input().split(" ").map(Number);
-  const arr = input().split(" ").map(Number);
-
+  const numbers = input().split(" ").map(Number);
   let result = Infinity;
+
   let start = 0;
   let end = 0;
   let sum = 0;
 
-  while (start <= end && end <= N) {
+  while (end <= N) {
     if (sum >= S) {
       result = Math.min(result, end - start);
-      sum -= arr[start];
+      sum -= numbers[start];
       start += 1;
     } else {
-      sum += arr[end];
+      sum += numbers[end];
       end += 1;
     }
   }
 
-  return result !== Infinity ? result : 0;
+  return result === Infinity ? 0 : result;
 };
+
+// 1차 해결
+// const solution = () => {
+//   const [N, S] = input().split(" ").map(Number);
+//   const arr = input().split(" ").map(Number);
+
+//   let result = Infinity;
+//   let start = 0;
+//   let end = 0;
+//   let sum = 0;
+
+//   while (start <= end && end <= N) {
+//     if (sum >= S) {
+//       result = Math.min(result, end - start);
+//       sum -= arr[start];
+//       start += 1;
+//     } else {
+//       sum += arr[end];
+//       end += 1;
+//     }
+//   }
+
+//   return result !== Infinity ? result : 0;
+// };
 
 console.log(solution());
