3c4ce59691daf430ed67446c9a12aa71c6afcb29
MyeongHoonNam <codenmh0822@naver.com>
2023-05-11 17:14:53 +0900

    문제 해결

---
+++ 투 포인터/22862.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `8 2
+1 2 3 4 5 6 7 8`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const solution = () => {
+  const [N, K] = input().split(" ").map(Number);
+  const S = input().split(" ").map(Number);
+  let result = 0;
+  let eraseCount = 0;
+
+  for (let start = 0, end = 0; start < N; start++) {
+    while (end < N) {
+      if (S[end] % 2 !== 0) {
+        eraseCount += 1;
+      }
+
+      end += 1;
+    }
+
+    result = Math.max(result, end - start - eraseCount);
+
+    if (S[start] % 2 === 1) {
+      eraseCount -= 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(solution());
