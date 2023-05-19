4967c242899485048d46e9949b959deadfd6f70d
MyeongHoonNam <codenmh0822@naver.com>
2023-05-19 19:03:43 +0900

    문제 2차 해결

--- 최단경로/1058.js
+++ 최단경로/1058.js
@@ -2,12 +2,10 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `5
-NYNNN
-YNYNN
-NYNYN
-NNYNY
-NNNYN`
+    : `3
+NYY
+YNY
+YYN`
 ).split("\n");
 
 const input = (() => {
@@ -15,9 +13,11 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const N = Number(input());
   const graph = Array.from(new Array(N), () => new Array(N).fill(Infinity));
+  let result = 0;
 
   for (let i = 0; i < N; i++) {
     const row = input().split("");
@@ -31,7 +31,9 @@ const solution = () => {
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (i === j) graph[i][j] = 0;
+      if (i === j) {
+        graph[i][j] = 0;
+      }
     }
   }
 
@@ -43,17 +45,8 @@ const solution = () => {
     }
   }
 
-  let result = 0;
-
   for (let i = 0; i < N; i++) {
-    let count = 0;
-
-    for (let j = 0; j < N; j++) {
-      if (graph[i][j] !== Infinity && graph[i][j] <= 2 && graph[i][j] > 0) {
-        count += 1;
-      }
-    }
-
+    const count = graph[i].filter((v) => 0 < v && v <= 2).length;
     result = Math.max(result, count);
   }
 
@@ -61,3 +54,51 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const N = Number(input());
+//   const graph = Array.from(new Array(N), () => new Array(N).fill(Infinity));
+
+//   for (let i = 0; i < N; i++) {
+//     const row = input().split("");
+
+//     for (let j = 0; j < N; j++) {
+//       if (row[j] === "Y") {
+//         graph[i][j] = 1;
+//       }
+//     }
+//   }
+
+//   for (let i = 0; i < N; i++) {
+//     for (let j = 0; j < N; j++) {
+//       if (i === j) graph[i][j] = 0;
+//     }
+//   }
+
+//   for (let k = 0; k < N; k++) {
+//     for (let i = 0; i < N; i++) {
+//       for (let j = 0; j < N; j++) {
+//         graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+//       }
+//     }
+//   }
+
+//   let result = 0;
+
+//   for (let i = 0; i < N; i++) {
+//     let count = 0;
+
+//     for (let j = 0; j < N; j++) {
+//       if (graph[i][j] !== Infinity && graph[i][j] <= 2 && graph[i][j] > 0) {
+//         count += 1;
+//       }
+//     }
+
+//     result = Math.max(result, count);
+//   }
+
+//   return result;
+// };
+
+// console.log(solution());
