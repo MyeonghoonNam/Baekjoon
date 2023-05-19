fed1b88146b21d74091745f2bdd5cb8a9af15fae
MyeongHoonNam <codenmh0822@naver.com>
2023-05-19 14:33:24 +0900

    문제 4차 해결 - 시간복잡도 개선

--- 최단경로/1753.js
+++ 최단경로/1753.js
@@ -17,7 +17,7 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-// 3차 해결
+// 4차 해결
 class MinHeap {
   constructor() {
     this.heap = [];
@@ -31,20 +31,19 @@ class MinHeap {
   }
 
   pop() {
-    if (this.isEmpty()) return;
+    if (this.size === 0) return;
 
     if (this.size === 1) {
       this.size -= 1;
       return this.heap.pop();
     }
 
-    const minValue = this.heap[0];
-
+    const popValue = this.heap[0];
     this.heap[0] = this.heap.pop();
     this.size -= 1;
     this.moveDown();
 
-    return minValue;
+    return popValue;
   }
 
   moveUp() {
@@ -65,14 +64,14 @@ class MinHeap {
     const rightChildIndex = currentIndex * 2 + 2;
     let minIndex = currentIndex;
 
-    while (
+    if (
       leftChildIndex < this.size &&
       this.heap[leftChildIndex].cost < this.heap[minIndex].cost
     ) {
       minIndex = leftChildIndex;
     }
 
-    while (
+    if (
       rightChildIndex < this.size &&
       this.heap[rightChildIndex].cost < this.heap[minIndex].cost
     ) {
@@ -85,14 +84,14 @@ class MinHeap {
     }
   }
 
-  isEmpty() {
-    return this.size === 0 ? true : false;
-  }
-
   swap(a, b) {
-    const temp = this.heap[a];
+    let temp = this.heap[a];
     this.heap[a] = this.heap[b];
     this.heap[b] = temp;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
   }
 }
 
@@ -103,34 +102,35 @@ const solution = () => {
   const distance = new Array(V + 1).fill(Infinity);
 
   for (let i = 0; i < E; i++) {
-    const [u, v, w] = input().split(" ").map(Number);
-    graph[u].push({ node: v, cost: w });
+    const [start, end, cost] = input().split(" ").map(Number);
+
+    graph[start].push({ node: end, cost });
   }
 
-  const dijkstra = (start) => {
+  const dijkstra = () => {
     const queue = new MinHeap();
 
-    queue.insert({ node: start, cost: 0 });
-    distance[start] = 0;
+    queue.insert({ node: K, cost: 0 });
+    distance[K] = 0;
 
     while (!queue.isEmpty()) {
-      const current = queue.pop();
+      const { node: start, cost: startCost } = queue.pop();
 
-      if (distance[current.node] < current.cost) continue;
+      if (distance[start] < startCost) continue;
 
-      for (let i = 0; i < graph[current.node].length; i++) {
-        const next = graph[current.node][i];
-        const totalCost = current.cost + next.cost;
+      for (let i = 0; i < graph[start].length; i++) {
+        const { node: end, cost: endCost } = graph[start][i];
+        const totalCost = startCost + endCost;
 
-        if (totalCost < distance[next.node]) {
-          queue.insert({ node: next.node, cost: totalCost });
-          distance[next.node] = totalCost;
+        if (totalCost < distance[end]) {
+          queue.insert({ node: end, cost: totalCost });
+          distance[end] = totalCost;
         }
       }
     }
   };
 
-  dijkstra(K);
+  dijkstra();
 
   const result = [];
 
@@ -144,6 +144,138 @@ const solution = () => {
 
   return result.join("\n");
 };
+
+console.log(solution());
+
+// 3차 해결
+// class MinHeap {
+//   constructor() {
+//     this.heap = [];
+//     this.size = 0;
+//   }
+
+//   insert(value) {
+//     this.heap.push(value);
+//     this.size += 1;
+//     this.moveUp();
+//   }
+
+//   pop() {
+//     if (this.isEmpty()) return;
+
+//     if (this.size === 1) {
+//       this.size -= 1;
+//       return this.heap.pop();
+//     }
+
+//     const minValue = this.heap[0];
+
+//     this.heap[0] = this.heap.pop();
+//     this.size -= 1;
+//     this.moveDown();
+
+//     return minValue;
+//   }
+
+//   moveUp() {
+//     let currentIndex = this.size - 1;
+
+//     while (currentIndex > 0) {
+//       const parentIndex = parseInt((currentIndex - 1) / 2);
+
+//       if (this.heap[parentIndex].cost <= this.heap[currentIndex].cost) break;
+
+//       this.swap(parentIndex, currentIndex);
+//       currentIndex = parentIndex;
+//     }
+//   }
+
+//   moveDown(currentIndex = 0) {
+//     const leftChildIndex = currentIndex * 2 + 1;
+//     const rightChildIndex = currentIndex * 2 + 2;
+//     let minIndex = currentIndex;
+
+//     while (
+//       leftChildIndex < this.size &&
+//       this.heap[leftChildIndex].cost < this.heap[minIndex].cost
+//     ) {
+//       minIndex = leftChildIndex;
+//     }
+
+//     while (
+//       rightChildIndex < this.size &&
+//       this.heap[rightChildIndex].cost < this.heap[minIndex].cost
+//     ) {
+//       minIndex = rightChildIndex;
+//     }
+
+//     if (minIndex !== currentIndex) {
+//       this.swap(minIndex, currentIndex);
+//       this.moveDown(minIndex);
+//     }
+//   }
+
+//   isEmpty() {
+//     return this.size === 0 ? true : false;
+//   }
+
+//   swap(a, b) {
+//     const temp = this.heap[a];
+//     this.heap[a] = this.heap[b];
+//     this.heap[b] = temp;
+//   }
+// }
+
+// const solution = () => {
+//   const [V, E] = input().split(" ").map(Number);
+//   const K = Number(input());
+//   const graph = Array.from(new Array(V + 1), () => []);
+//   const distance = new Array(V + 1).fill(Infinity);
+
+//   for (let i = 0; i < E; i++) {
+//     const [u, v, w] = input().split(" ").map(Number);
+//     graph[u].push({ node: v, cost: w });
+//   }
+
+//   const dijkstra = (start) => {
+//     const queue = new MinHeap();
+
+//     queue.insert({ node: start, cost: 0 });
+//     distance[start] = 0;
+
+//     while (!queue.isEmpty()) {
+//       const current = queue.pop();
+
+//       if (distance[current.node] < current.cost) continue;
+
+//       for (let i = 0; i < graph[current.node].length; i++) {
+//         const next = graph[current.node][i];
+//         const totalCost = current.cost + next.cost;
+
+//         if (totalCost < distance[next.node]) {
+//           queue.insert({ node: next.node, cost: totalCost });
+//           distance[next.node] = totalCost;
+//         }
+//       }
+//     }
+//   };
+
+//   dijkstra(K);
+
+//   const result = [];
+
+//   for (let i = 1; i <= V; i++) {
+//     if (distance[i] === Infinity) {
+//       result.push("INF");
+//     } else {
+//       result.push(distance[i]);
+//     }
+//   }
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
 
 // 2차 해결
 // class MinHeap {
@@ -270,8 +402,9 @@ const solution = () => {
 //   return result.join("\n");
 // };
 
-console.log(solution());
+// console.log(solution());
 
+// 1차 해결
 // class MinHeap {
 //   constructor() {
 //     this.heap = [];
