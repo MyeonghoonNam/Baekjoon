e374098b68d38f79ffa61fa628ed994fd24f6a7c
MyeongHoonNam <codenmh0822@naver.com>
2023-05-28 22:01:48 +0900

    문제 2차 해결 - 순열

--- 백트래킹/15649.js
+++ 백트래킹/15649.js
@@ -10,12 +10,13 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
   const visited = new Array(N).fill(false);
   const selected = [];
-  let result = [];
+  const result = [];
 
   const dfs = (cnt) => {
     if (cnt === M) {
@@ -29,8 +30,8 @@ const solution = () => {
       selected.push(numbers[i]);
       visited[i] = true;
       dfs(cnt + 1);
-      selected.pop();
       visited[i] = false;
+      selected.pop();
     }
   };
 
@@ -40,3 +41,35 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+//   const visited = new Array(N).fill(false);
+//   const selected = [];
+//   let result = [];
+
+//   const dfs = (cnt) => {
+//     if (cnt === M) {
+//       result.push(selected.join(" "));
+//       return;
+//     }
+
+//     for (let i = 0; i < N; i++) {
+//       if (visited[i]) continue;
+
+//       selected.push(numbers[i]);
+//       visited[i] = true;
+//       dfs(cnt + 1);
+//       selected.pop();
+//       visited[i] = false;
+//     }
+//   };
+
+//   dfs(0);
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
