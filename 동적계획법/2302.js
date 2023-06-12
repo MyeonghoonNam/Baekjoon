68252bcd453bffcac4feb50d98770755dd4b5c14
MyeongHoonNam <codenmh0822@naver.com>
2023-06-12 17:39:25 +0900

    문제 2차 해결

--- 동적계획법/2302.js
+++ 동적계획법/2302.js
@@ -13,27 +13,23 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const N = Number(input());
   const M = Number(input());
-  const DP = [];
-
-  DP[0] = 1;
-  DP[1] = 1;
-  DP[2] = 2;
+  const DP = [1, 1, 2];
+  const fixedSeat = [];
+  const cases = [];
+  let start = 0;
+  let result = 1;
 
   for (let i = 3; i <= N; i++) {
     DP[i] = DP[i - 1] + DP[i - 2];
   }
 
-  const fixedSeat = [];
-
   for (let i = 0; i < M; i++) {
     fixedSeat.push(Number(input()));
   }
-
-  const cases = [];
-  let start = 0;
 
   for (let i = 0; i < M; i++) {
     const end = fixedSeat[i];
@@ -44,14 +40,55 @@ const solution = () => {
 
   cases.push(N - start);
 
-  let result = 1;
-
   for (let i = 0; i < cases.length; i++) {
-    const count = cases[i];
-    result *= DP[count];
+    result *= DP[cases[i]];
   }
 
   return result;
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const N = Number(input());
+//   const M = Number(input());
+//   const DP = [];
+
+//   DP[0] = 1;
+//   DP[1] = 1;
+//   DP[2] = 2;
+
+//   for (let i = 3; i <= N; i++) {
+//     DP[i] = DP[i - 1] + DP[i - 2];
+//   }
+
+//   const fixedSeat = [];
+
+//   for (let i = 0; i < M; i++) {
+//     fixedSeat.push(Number(input()));
+//   }
+
+//   const cases = [];
+//   let start = 0;
+
+//   for (let i = 0; i < M; i++) {
+//     const end = fixedSeat[i];
+
+//     cases.push(end - 1 - start);
+//     start = end;
+//   }
+
+//   cases.push(N - start);
+
+//   let result = 1;
+
+//   for (let i = 0; i < cases.length; i++) {
+//     const count = cases[i];
+//     result *= DP[count];
+//   }
+
+//   return result;
+// };
+
+// console.log(solution());
