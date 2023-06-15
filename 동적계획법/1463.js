436efd9b03134894111587d2d3f88ad75e3569e6
MyeongHoonNam <codenmh0822@naver.com>
2023-06-15 20:13:58 +0900

    문제 3차 해결

--- 동적계획법/1463.js
+++ 동적계획법/1463.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const stdin = (
-  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `10`
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`
 ).split("\n");
 
 const input = (() => {
@@ -13,30 +13,8 @@ const input = (() => {
  *
  * 같은 연산에 대한 접근이 무수히 많이 이루어지는 구조이므로 DP를 활용
  */
-// const solution = () => {
-//   const N = Number(input());
-//   const DP = [0, 0];
 
-//   for (let i = 2; i <= N; i++) {
-//     DP[i] = DP[i - 1] + 1;
-
-//     if (i % 2 === 0) {
-//       DP[i] = Math.min(DP[i], DP[i / 2] + 1);
-//     }
-
-//     if (i % 3 === 0) {
-//       DP[i] = Math.min(DP[i], DP[i / 3] + 1);
-//     }
-//   }
-
-//   return DP[N];
-// };
-
-// 문제 2차 해결
-/**
- * 시간제한 0.15초
- * 입력값 10^6 = 1,000,000
- */
+// 3차 해결
 const solution = () => {
   const N = Number(input());
   const DP = [0, 0];
@@ -57,3 +35,47 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 2차 해결
+// const solution = () => {
+//   const N = Number(input());
+//   const DP = [0, 0];
+
+//   for (let i = 2; i <= N; i++) {
+//     DP[i] = DP[i - 1] + 1;
+
+//     if (i % 3 === 0) {
+//       DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+//     }
+
+//     if (i % 2 === 0) {
+//       DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+//     }
+//   }
+
+//   return DP[N];
+// };
+
+// console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const N = Number(input());
+//   const DP = [0, 0];
+
+//   for (let i = 2; i <= N; i++) {
+//     DP[i] = DP[i - 1] + 1;
+
+//     if (i % 2 === 0) {
+//       DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+//     }
+
+//     if (i % 3 === 0) {
+//       DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+//     }
+//   }
+
+//   return DP[N];
+// };
+
+// console.log(solution());
