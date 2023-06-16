5dfdb1743ed7fd2bbb4974a6601d1659b86b6870
MyeongHoonNam <codenmh0822@naver.com>
2023-06-16 17:51:01 +0900

    문제 해결

--- 동적계획법/10844.js
+++ 동적계획법/10844.js
@@ -1,49 +1,51 @@
-const readline = require('readline');
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`
+).split("\n");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
 
-const input = [];
-rl.on('line', (line) => {
-  // 입력 관리
-  input.push(line);
-}).on('close', () => {
-  // 구현
-  console.log(Solution(input));
+const solution = () => {
+  const N = Number(input());
+  const DP = Array.from(new Array(N + 1), () => new Array(10).fill(0));
 
-  function Solution(input) {
-    const N = Number(input[0]);
-    const MOD = 1000000000;
+  // i자리 수의 마지막 숫자가 j인 경우
+  // 0과 9는 예외처리 필요
+  // DP[i][j] = DP[i-1][j-1] + DP[i-1][j+1]
 
-    const dp = Array.from(new Array(N + 1), () => new Array(10).fill(0));
+  DP[1][0] = 0;
 
-    for (let i = 1; i <= 9; i++) {
-      dp[1][i] = 1;
-    }
-
-    for (let i = 2; i <= N; i++) {
-      // 마지막 자리 : 0인 경우
-      dp[i][0] = dp[i - 1][1];
-
-      for (let j = 1; j <= 9; j++) {
-        // 마지막 자리 : 9인 경우
-        if (j === 9) {
-          dp[i][j] = dp[i - 1][8];
-        } else {
-          // 마지막 자리 : 1~8인 경우
-          dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-        }
-
-        dp[i][j] = dp[i][j] % MOD;
-      }
-    }
-
-    const result = dp[N].reduce((acc, cur) => {
-      return acc + cur;
-    });
-
-    return result % MOD;
+  for (let i = 1; i <= 9; i++) {
+    DP[1][i] = 1;
   }
-});
+
+  for (let i = 2; i <= N; i++) {
+    for (let j = 0; j <= 9; j++) {
+      if (j === 0) {
+        DP[i][0] = DP[i - 1][1];
+      } else if (j >= 1 && j <= 8) {
+        // 1 ~ 8
+        DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j + 1];
+      } else {
+        // 9
+        DP[i][9] = DP[i - 1][8];
+      }
+
+      DP[i][j] %= Number(1e9);
+    }
+  }
+
+  let result = 0;
+
+  for (let i = 0; i <= 9; i++) {
+    result += DP[N][i];
+    result %= Number(1e9);
+  }
+
+  return result;
+};
+
+console.log(solution());
