66163717871fc853a195be080b4b32f789b99fff
MyeongHoonNam <codenmh0822@naver.com>
2023-06-19 22:43:13 +0900

    문제 2차 해결

--- 백트래킹/15650.js
+++ 백트래킹/15650.js
@@ -10,6 +10,7 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
@@ -40,3 +41,35 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+//   const visited = new Array(N).fill(false);
+//   const selected = [];
+//   const result = [];
+
+//   const dfs = (idx, cnt) => {
+//     if (cnt === M) {
+//       result.push(selected.join(" "));
+//       return;
+//     }
+
+//     for (let i = idx; i < N; i++) {
+//       if (visited[i]) continue;
+
+//       visited[i] = true;
+//       selected.push(numbers[i]);
+//       dfs(i, cnt + 1);
+//       selected.pop();
+//       visited[i] = false;
+//     }
+//   };
+
+//   dfs(0, 0);
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
