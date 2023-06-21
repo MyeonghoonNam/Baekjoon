c22417ad1bdacfe39ec78d466f9e33a3de778ba0
MyeongHoonNam <codenmh0822@naver.com>
2023-06-21 16:41:24 +0900

    문제 2차 해결

--- 그리디/1541.js
+++ 그리디/1541.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `55-50+40`
+    : `00009-00009`
 ).split("\n");
 
 const input = (() => {
@@ -10,12 +10,13 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
-  const groups = input().split("-");
+  const expression = input().split("-");
   let result = 0;
 
-  for (let i = 0; i < groups.length; i++) {
-    const sum = groups[i]
+  for (let i = 0; i < expression.length; i++) {
+    const sum = expression[i]
       .split("+")
       .map(Number)
       .reduce((acc, cur) => acc + cur);
@@ -27,3 +28,22 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const groups = input().split("-");
+//   let result = 0;
+
+//   for (let i = 0; i < groups.length; i++) {
+//     const sum = groups[i]
+//       .split("+")
+//       .map(Number)
+//       .reduce((acc, cur) => acc + cur);
+
+//     i === 0 ? (result += sum) : (result -= sum);
+//   }
+
+//   return result;
+// };
+
+// console.log(solution());
