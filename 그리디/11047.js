a3152507bff8da38c1702b7bdcda95bd26e18fad
MyeongHoonNam <codenmh0822@naver.com>
2023-06-21 16:24:04 +0900

    문제 2차 해결

--- 그리디/11047.js
+++ 그리디/11047.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `10 4790
+    : `10 4200
 1
 5
 10
@@ -20,23 +20,52 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
-  let [N, K] = input().split(" ").map(Number);
+  const [N, K] = input().split(" ").map(Number);
   const coins = [];
+  let remain = K;
   let result = 0;
 
   for (let i = 0; i < N; i++) {
-    coins.push(Number(input()));
+    const coin = Number(input());
+
+    if (coin <= K) {
+      coins.push(coin);
+    }
   }
 
-  for (let i = N - 1; i >= 0; i--) {
+  for (let i = coins.length - 1; i >= 0; i--) {
     const coin = coins[i];
+    const count = parseInt(remain / coin);
 
-    result += parseInt(K / coin);
-    K %= coins[i];
+    remain %= coin;
+    result += count;
   }
 
   return result;
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   let [N, K] = input().split(" ").map(Number);
+//   const coins = [];
+//   let result = 0;
+
+//   for (let i = 0; i < N; i++) {
+//     coins.push(Number(input()));
+//   }
+
+//   for (let i = N - 1; i >= 0; i--) {
+//     const coin = coins[i];
+
+//     result += parseInt(K / coin);
+//     K %= coins[i];
+//   }
+
+//   return result;
+// };
+
+// console.log(solution());
