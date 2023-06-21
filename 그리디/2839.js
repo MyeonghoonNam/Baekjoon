72d386d8da43fd765fde2e7ef5add4bedd37e714
MyeongHoonNam <codenmh0822@naver.com>
2023-06-21 18:08:28 +0900

    문제 2차 해결 - 동적계획법

--- 그리디/2839.js
+++ 그리디/2839.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const stdin = (
-  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `11`
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `18`
 ).split("\n");
 
 const input = (() => {
@@ -8,26 +8,49 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
-  let N = Number(input());
-  let result = 0;
-  let fiveBag = 0;
-  let threeBag = 0;
+  const N = Number(input());
 
-  while (true) {
-    if (N < 0) {
-      return -1;
-    }
+  if (N < 5 && N !== 3) return -1;
 
-    if (N % 5 === 0) {
-      fiveBag += N / 5;
-      result += fiveBag + threeBag;
-      return result;
-    }
+  const DP = new Array(N + 1).fill(Infinity);
 
-    N -= 3;
-    threeBag += 1;
+  DP[3] = 1;
+  DP[5] = 1;
+
+  for (let i = 6; i <= N; i++) {
+    DP[i] = Math.min(DP[i - 3], DP[i - 5]) + 1;
   }
+
+  const result = DP[N] === Infinity ? -1 : DP[N];
+
+  return result;
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   let N = Number(input());
+//   let result = 0;
+//   let fiveBag = 0;
+//   let threeBag = 0;
+
+//   while (true) {
+//     if (N < 0) {
+//       return -1;
+//     }
+
+//     if (N % 5 === 0) {
+//       fiveBag += N / 5;
+//       result += fiveBag + threeBag;
+//       return result;
+//     }
+
+//     N -= 3;
+//     threeBag += 1;
+//   }
+// };
+
+// console.log(solution());
