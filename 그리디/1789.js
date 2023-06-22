27b83981be81b5a0008321ee91532d110192f685
MyeongHoonNam <codenmh0822@naver.com>
2023-06-22 14:24:31 +0900

    문제 2차 해결

--- 그리디/1789.js
+++ 그리디/1789.js
@@ -10,11 +10,11 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const S = Number(input());
-
-  let sum = 0;
   let currentNumber = 0;
+  let sum = 0;
 
   while (sum <= S) {
     currentNumber += 1;
@@ -27,3 +27,22 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const S = Number(input());
+
+//   let sum = 0;
+//   let currentNumber = 0;
+
+//   while (sum <= S) {
+//     currentNumber += 1;
+//     sum += currentNumber;
+//   }
+
+//   const result = currentNumber - 1;
+
+//   return result;
+// };
+
+// console.log(solution());
