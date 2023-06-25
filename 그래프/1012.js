de2a21a7977a1964543b4c0f872f01efca85653f
MyeongHoonNam <codenmh0822@naver.com>
2023-06-25 14:20:26 +0900

    문제 3차 해결

--- 그래프/1012.js
+++ 그래프/1012.js
@@ -30,13 +30,14 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-// 2차 해결
+// 3차 해결
 const solution = () => {
   const result = [];
   let T = Number(input());
+
   while (T--) {
     const [M, N, K] = input().split(" ").map(Number);
-    const graph = Array.from(new Array(N), () => new Array(M).fill(0));
+    const map = Array.from(new Array(N), () => new Array(M).fill(0));
     const visited = Array.from(new Array(N), () => new Array(M).fill(false));
     const dx = [0, 0, -1, 1];
     const dy = [-1, 1, 0, 0];
@@ -44,7 +45,7 @@ const solution = () => {
 
     for (let i = 0; i < K; i++) {
       const [X, Y] = input().split(" ").map(Number);
-      graph[Y][X] = 1;
+      map[Y][X] = 1;
     }
 
     const dfs = (y, x) => {
@@ -56,7 +57,7 @@ const solution = () => {
 
         if (!checkMapRange(ny, nx)) continue;
 
-        if (!visited[ny][nx] && graph[ny][nx] === 1) {
+        if (!visited[ny][nx] && map[ny][nx] === 1) {
           dfs(ny, nx);
         }
       }
@@ -69,7 +70,7 @@ const solution = () => {
 
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (!visited[i][j] && graph[i][j] === 1) {
+        if (!visited[i][j] && map[i][j] === 1) {
           dfs(i, j);
           count += 1;
         }
@@ -81,6 +82,60 @@ const solution = () => {
 
   return result.join("\n");
 };
+
+console.log(solution());
+
+// 2차 해결
+// const solution = () => {
+//   const result = [];
+//   let T = Number(input());
+//   while (T--) {
+//     const [M, N, K] = input().split(" ").map(Number);
+//     const graph = Array.from(new Array(N), () => new Array(M).fill(0));
+//     const visited = Array.from(new Array(N), () => new Array(M).fill(false));
+//     const dx = [0, 0, -1, 1];
+//     const dy = [-1, 1, 0, 0];
+//     let count = 0;
+
+//     for (let i = 0; i < K; i++) {
+//       const [X, Y] = input().split(" ").map(Number);
+//       graph[Y][X] = 1;
+//     }
+
+//     const dfs = (y, x) => {
+//       visited[y][x] = true;
+
+//       for (let i = 0; i < 4; i++) {
+//         const nx = x + dx[i];
+//         const ny = y + dy[i];
+
+//         if (!checkMapRange(ny, nx)) continue;
+
+//         if (!visited[ny][nx] && graph[ny][nx] === 1) {
+//           dfs(ny, nx);
+//         }
+//       }
+//     };
+
+//     const checkMapRange = (y, x) => {
+//       if (x >= 0 && y >= 0 && x < M && y < N) return true;
+//       else return false;
+//     };
+
+//     for (let i = 0; i < N; i++) {
+//       for (let j = 0; j < M; j++) {
+//         if (!visited[i][j] && graph[i][j] === 1) {
+//           dfs(i, j);
+//           count += 1;
+//         }
+//       }
+//     }
+
+//     result.push(count);
+//   }
+
+//   return result.join("\n");
+// };
 
 // 1차 해결
 // const solution = () => {
