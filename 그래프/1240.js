8ef54e3cb47ef9e0d6a4a12bb28bd15dee6530b8
MyeongHoonNam <codenmh0822@naver.com>
2023-06-26 20:03:09 +0900

    문제 3차 해결

--- 그래프/1240.js
+++ 그래프/1240.js
@@ -15,30 +15,33 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-// 2차 해결 distance 거리 배열 활용
+// 3차 해결
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
   const graph = Array.from(new Array(N + 1), () => []);
   let visited = new Array(N + 1).fill(false);
-  let distance = new Array(N + 1).fill(Infinity);
   const result = [];
 
   for (let i = 0; i < N - 1; i++) {
     const [start, end, dist] = input().split(" ").map(Number);
 
-    graph[start].push({ node: end, dist });
-    graph[end].push({ node: start, dist });
+    graph[start].push([end, dist]);
+    graph[end].push([start, dist]);
   }
 
-  const dfs = (node, dist) => {
-    visited[node] = true;
-    distance[node] = dist;
+  const dfs = (current, end, dist) => {
+    visited[current] = true;
 
-    for (let i = 0; i < graph[node].length; i++) {
-      const next = graph[node][i];
+    if (current === end) {
+      result.push(dist);
+      return;
+    }
 
-      if (!visited[next.node]) {
-        dfs(next.node, dist + next.dist);
+    for (let i = 0; i < graph[current].length; i++) {
+      const [next, cost] = graph[current][i];
+
+      if (!visited[next]) {
+        dfs(next, end, dist + cost);
       }
     }
   };
@@ -46,14 +49,54 @@ const solution = () => {
   for (let i = 0; i < M; i++) {
     const [start, end] = input().split(" ").map(Number);
 
-    dfs(start, 0);
-    result.push(distance[end]);
+    dfs(start, end, 0);
     visited = new Array(N + 1).fill(false);
-    distance = new Array(N + 1).fill(Infinity);
   }
 
   return result.join("\n");
 };
+
+console.log(solution());
+
+// 2차 해결 distance 거리 배열 활용
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const graph = Array.from(new Array(N + 1), () => []);
+//   let visited = new Array(N + 1).fill(false);
+//   let distance = new Array(N + 1).fill(Infinity);
+//   const result = [];
+
+//   for (let i = 0; i < N - 1; i++) {
+//     const [start, end, dist] = input().split(" ").map(Number);
+
+//     graph[start].push({ node: end, dist });
+//     graph[end].push({ node: start, dist });
+//   }
+
+//   const dfs = (node, dist) => {
+//     visited[node] = true;
+//     distance[node] = dist;
+
+//     for (let i = 0; i < graph[node].length; i++) {
+//       const next = graph[node][i];
+
+//       if (!visited[next.node]) {
+//         dfs(next.node, dist + next.dist);
+//       }
+//     }
+//   };
+
+//   for (let i = 0; i < M; i++) {
+//     const [start, end] = input().split(" ").map(Number);
+
+//     dfs(start, 0);
+//     result.push(distance[end]);
+//     visited = new Array(N + 1).fill(false);
+//     distance = new Array(N + 1).fill(Infinity);
+//   }
+
+//   return result.join("\n");
+// };
 
 // 1차 해결
 // const solution = () => {
@@ -96,4 +139,4 @@ const solution = () => {
 //   return result.join("\n");
 // };
 
-console.log(solution());
+// console.log(solution());
