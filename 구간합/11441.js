e3b7df9cb0f67f14dee1d3c65b6f77b46e3c1942
MyeongHoonNam <codenmh0822@naver.com>
2023-07-03 22:29:37 +0900

    문제 2차 해결

--- 구간합/11441.js
+++ 구간합/11441.js
@@ -17,6 +17,7 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 풀이
 const solution = () => {
   const N = Number(input());
   const numbers = input().split(" ").map(Number);
@@ -41,3 +42,29 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 풀이
+// const solution = () => {
+//   const N = Number(input());
+//   const numbers = input().split(" ").map(Number);
+//   const M = Number(input());
+
+//   const prefixSum = [0];
+//   let sum = 0;
+
+//   for (let i = 0; i < N; i++) {
+//     sum += numbers[i];
+//     prefixSum.push(sum);
+//   }
+
+//   const result = [];
+
+//   for (let i = 0; i < M; i++) {
+//     const [start, end] = input().split(" ").map(Number);
+//     result.push(prefixSum[end] - prefixSum[start - 1]);
+//   }
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
