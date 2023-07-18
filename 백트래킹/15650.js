77b40b37ea487b3a46a0f50f57c7dd6ccbbe441b
MyeongHoonNam <codenmh0822@naver.com>
2023-07-18 14:42:46 +0900

    문제 3차 해결 - 조합

--- 백트래킹/15650.js
+++ 백트래킹/15650.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `4 2`
+    : `4 4`
 ).split("\n");
 
 const input = (() => {
@@ -10,13 +10,17 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-// 2차 해결
+// 3차 해결
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
-  const numbers = new Array(N).fill(0).map((_, i) => i + 1);
-  const visited = new Array(N).fill(false);
+  const numbers = [];
   const selected = [];
+  const visited = new Array(N + 1).fill(false);
   const result = [];
+
+  for (let i = 1; i <= N; i += 1) {
+    numbers.push(i);
+  }
 
   const dfs = (idx, cnt) => {
     if (cnt === M) {
@@ -24,14 +28,14 @@ const solution = () => {
       return;
     }
 
-    for (let i = idx; i < N; i++) {
-      if (visited[i]) continue;
-
-      visited[i] = true;
-      selected.push(numbers[i]);
-      dfs(i, cnt + 1);
-      selected.pop();
-      visited[i] = false;
+    for (let i = idx; i < N; i += 1) {
+      if (!visited[i]) {
+        visited[i] = true;
+        selected.push(numbers[i]);
+        dfs(i, cnt + 1);
+        selected.pop();
+        visited[i] = false;
+      }
     }
   };
 
@@ -41,6 +45,38 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 2차 해결
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+//   const visited = new Array(N).fill(false);
+//   const selected = [];
+//   const result = [];
+
+//   const dfs = (idx, cnt) => {
+//     if (cnt === M) {
+//       result.push(selected.join(" "));
+//       return;
+//     }
+
+//     for (let i = idx; i < N; i++) {
+//       if (visited[i]) continue;
+
+//       visited[i] = true;
+//       selected.push(numbers[i]);
+//       dfs(i, cnt + 1);
+//       selected.pop();
+//       visited[i] = false;
+//     }
+//   };
+
+//   dfs(0, 0);
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
 
 // 1차 해결
 // const solution = () => {
