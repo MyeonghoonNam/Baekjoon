264ec07d7293d54b398f99267121717d19eba50b
MyeongHoonNam <codenmh0822@naver.com>
2023-07-18 14:53:58 +0900

    문제 2차 해결 - 중복조합

--- 백트래킹/15652.js
+++ 백트래킹/15652.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const stdin = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
-    : `4 2`
+    : `3 3`
 ).split("\n");
 
 const input = (() => {
@@ -10,11 +10,16 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+// 2차 해결
 const solution = () => {
   const [N, M] = input().split(" ").map(Number);
-  const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+  const numbers = [];
   const selected = [];
   const result = [];
+
+  for (let i = 1; i <= N; i += 1) {
+    numbers.push(i);
+  }
 
   const dfs = (idx, cnt) => {
     if (cnt === M) {
@@ -22,9 +27,10 @@ const solution = () => {
       return;
     }
 
-    for (let i = idx; i < N; i++) {
-      selected[cnt] = numbers[i];
+    for (let i = idx; i < N; i += 1) {
+      selected.push(numbers[i]);
       dfs(i, cnt + 1);
+      selected.pop();
     }
   };
 
@@ -34,3 +40,29 @@ const solution = () => {
 };
 
 console.log(solution());
+
+// 1차 해결
+// const solution = () => {
+//   const [N, M] = input().split(" ").map(Number);
+//   const numbers = new Array(N).fill(0).map((_, i) => i + 1);
+//   const selected = [];
+//   const result = [];
+
+//   const dfs = (idx, cnt) => {
+//     if (cnt === M) {
+//       result.push(selected.join(" "));
+//       return;
+//     }
+
+//     for (let i = idx; i < N; i++) {
+//       selected[cnt] = numbers[i];
+//       dfs(i, cnt + 1);
+//     }
+//   };
+
+//   dfs(0, 0);
+
+//   return result.join("\n");
+// };
+
+// console.log(solution());
